8399de7ccedd7649fbf98600a8baf911ee491aff
plouiserre <plouiserre@gmail.com>
2026-04-02 16:18:30 +0200

    clean localisation code

--- GameOfLife/tests/test_manipulator.py
+++ GameOfLife/tests/test_manipulator.py
@@ -1,37 +1,30 @@
 import math 
+from GameOfLife.manipulator import Manipulator
+
 
 simple_grid = [".*.", "**.", "..."]
 complexe_grid = ["........", "...**...", "...**...", "........"]
 
-def test_1():
+def test_locate_first_case_small_grid():
     assert locate_me(0,simple_grid) == [0, 0]
 
-def test_2(): 
+def test_locate_fourth_case_small_grid(): 
     assert locate_me(4, simple_grid) == [1, 1]
 
-def test_3(): 
+def test_locate_eighth_case_small_grid(): 
     assert locate_me(8, simple_grid) == [2, 2]
 
-def test_4(): 
+def test_locate_first_case_big_grid(): 
     assert locate_me(0, complexe_grid) == [0,0]
 
-def test_5(): 
+def test_locate_twelfth_case_small_grid(): 
     assert locate_me(12, complexe_grid) == [1,4]
 
-def test_6(): 
+def test_locate_twenty_eighth_case_small_grid(): 
     assert locate_me(28, complexe_grid) == [3,4]
 
 
 def locate_me(cell_position, grid) :    
-    coordonnates = []
-    for i, line in enumerate(grid) :
-        for j, column in enumerate(line) : 
-            if cell_position == 0 : 
-                coordonnates.append(i)
-                coordonnates.append(j)
-                break
-            else : 
-                cell_position =cell_position - 1
-        if cell_position == 0 : 
-            break         
+    manipulator = Manipulator(grid)
+    coordonnates = manipulator.locate_cell(cell_position)
     return coordonnates
