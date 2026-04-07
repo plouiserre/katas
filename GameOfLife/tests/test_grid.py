42538897c19b76edb5803eba8cb352c0f6a1818a
plouiserre <plouiserre@gmail.com>
2026-04-07 14:51:37 +0200

    mv alive neighbourgs cells tests  in grid test

--- GameOfLife/tests/test_grid.py
+++ GameOfLife/tests/test_grid.py
@@ -18,9 +18,20 @@ def test_get_second_alive_cell_content_simple_grid():
 def test_get_tenth_eleventh_twentieth_content_complexe_grid(): 
     assert content_retrieve([10,11, 20], complexe_grid) == [DEAD, ALIVE, ALIVE]
 
-
 def content_retrieve(cells, grid) : 
     location = Locator(grid)
     grid = Grid(location, grid)
     states = grid.get_contents_cells(cells)
     return states
+
+def test_count_alive_neighbourgs_second_cell_simple_grid():    
+    assert count_alive_neighbourgs_cells(1, simple_grid) == 2
+
+
+def test_count_alive_neighbourgs_twelveth_cell_complexe_grid(): 
+    assert count_alive_neighbourgs_cells(11, complexe_grid) == 3    
+
+def count_alive_neighbourgs_cells(cell_number, grid_content):
+    locator = Locator(grid_content)
+    grid = Grid(locator, grid_content)
+    return grid.count_alive_neighbourgs(cell_number)    
