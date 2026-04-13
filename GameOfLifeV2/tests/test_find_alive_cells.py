a0826bf50101a19ab4e98b6c0ecdb9fe51a478e0
plouiserre <plouiserre@gmail.com>
2026-04-13 11:21:28 +0200

    rename tests

--- GameOfLifeV2/tests/test_find_alive_cells.py
+++ GameOfLifeV2/tests/test_find_alive_cells.py
@@ -3,10 +3,10 @@ simple_grid = [".*.", "**.", "..."]
 complexe_grid = ["........", "...**...", "...**...", "........"]
 
 
-def test_1(): 
+def test_get_alive_cell_for_simple_grid(): 
     assert(get_alive_cells(simple_grid) == [[0,1], [1,0], [1,1]])
 
-def test_2(): 
+def test_get_alive_cells_for_complexe_grid(): 
     assert(get_alive_cells(complexe_grid) == [[1,3], [1,4], [2,3], [2,4]])
 
 
