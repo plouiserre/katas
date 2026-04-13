e3eaf3b159624eaec7094766c09fc4662ca07dc9
plouiserre <plouiserre@gmail.com>
2026-04-13 14:36:53 +0200

    mv code in grid class

--- GameOfLifeV2/grid.py
+++ GameOfLifeV2/grid.py
@@ -4,6 +4,21 @@ ALIVE = 1
 class Grid : 
     def __init__(self, grid_data):
         self.grid_data = grid_data
+
+    def count_alive_neighbourgs(self, coordonnates): 
+        alive_neighbourgs = 0
+        alive_cells = self.determinate_alive_cells()
+        for cells in alive_cells : 
+            y = coordonnates[0]
+            x = coordonnates[1]
+            cell_y = cells[0]
+            cell_x = cells[1]
+            for i in range(y - 1, y + 2 ) : 
+                for j in range (x - 1 , x + 2 ) :
+                    same_coordonnates = y == i and x == j
+                    if i == cell_y and j == cell_x  and same_coordonnates == False: 
+                        alive_neighbourgs +=1  
+        return alive_neighbourgs
         
     def get_status_cell_next_round(self, actual_status, alive_neighbourgs): 
         if actual_status == ALIVE and alive_neighbourgs < 2 : 
--- GameOfLifeV2/tests/test_a.py
+++ GameOfLifeV2/tests/test_a.py
@@ -3,7 +3,6 @@ from GameOfLifeV2.grid import Grid
 simple_grid = [".*.", "**.", "..."]
 complexe_grid = ["........", "...**...", "...**...", "........"]
 
-
 def test_1(): 
     assert count_alive_neighbourgs([0,0], simple_grid) == 3
 
@@ -14,17 +13,5 @@ def test_3():
     assert count_alive_neighbourgs([3,3], complexe_grid) == 2
 
 def count_alive_neighbourgs(coordonnates, grid_data): 
-    alive_neighbourgs = 0
-    grid = Grid(grid_data)
-    alive_cells = grid.determinate_alive_cells()
-    for cells in alive_cells : 
-        y = coordonnates[0]
-        x = coordonnates[1]
-        cell_y = cells[0]
-        cell_x = cells[1]
-        for i in range(y - 1, y + 2 ) : 
-            for j in range (x - 1 , x + 2 ) :
-                same_coordonnates = y == i and x == j
-                if i == cell_y and j == cell_x  and same_coordonnates == False: 
-                    alive_neighbourgs +=1  
-    return alive_neighbourgs
+    grid = Grid(grid_data)    
+    return grid.count_alive_neighbourgs(coordonnates)
