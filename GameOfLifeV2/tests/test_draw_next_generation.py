6220a5970ed1f41746d535816f5846c0ac15bbd1
plouiserre <plouiserre@gmail.com>
2026-04-15 10:36:48 +0200

    simplify code and remove useless test

--- GameOfLifeV2/tests/test_draw_next_generation.py
+++ GameOfLifeV2/tests/test_draw_next_generation.py
@@ -1,14 +1,19 @@
 from GameOfLifeV2.grid import Grid
 
 simple_grid = [".*.", "**.", "..."]
+medium_grid = ["*.*", ".*.", "..*"]
 complexe_grid = ["........", "...**...", "...**...", "........"]
 
-def test_draw_next_generation_simple_grid() : 
+def test_draw_first_generation_simple_grid() : 
     assert(draw_next_grid_generation(simple_grid) == ["**.", "**.", "..."])
 
-def test_draw_next_generation_complexe_grid(): 
+def test_draw_first_generation_medium_grid() : 
+    assert(draw_next_grid_generation(medium_grid) == [".*.", ".**", "..."])
+
+def test_draw_first_generation_complexe_grid(): 
     assert(draw_next_grid_generation(complexe_grid) == ["........", "...**...", "...**...", "........"])
- 
+
+
 def draw_next_grid_generation(grid_data):
     grid = Grid(grid_data)
     return grid.draw_next_generation()
