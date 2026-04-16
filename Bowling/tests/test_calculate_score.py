ebae5cc867566d20697a264e077c7637770ef4e0
plouiserre <plouiserre@gmail.com>
2026-04-16 11:54:37 +0200

    spare code did

--- Bowling/tests/test_calculate_score.py
+++ Bowling/tests/test_calculate_score.py
@@ -12,6 +12,19 @@ def test_eight_turns_with_nothing_special():
     turns = [[5,2], [6,3], [4,1], [0,6], [5,3], [4,3], [4,4], [2,6]]
     assert (calculate_score(turns) == 58)
 
+def test_eight_turns_with_spare(): 
+    turns = [[5,2], [6,3], [4,1], [0,6], [5,3], [4,3], [4,6], [2,6]]
+    assert (calculate_score(turns) == 62)
+
 def calculate_score(turns): 
-    score = Score(turns)
-    return score.Calculate()
+    score = 0
+    is_spare = False 
+    for turn in turns : 
+        if is_spare : 
+            score += turn[0]
+            is_spare = False
+        if turn[0]+turn[1] == 10 :            
+            is_spare = True
+        score += turn[0]
+        score += turn[1]        
+    return score
