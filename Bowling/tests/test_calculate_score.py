36c55527b2192647ca3814c660a2e0c24e666178
plouiserre <plouiserre@gmail.com>
2026-04-16 11:41:06 +0200

    code resolve

--- Bowling/tests/test_calculate_score.py
+++ Bowling/tests/test_calculate_score.py
@@ -1,14 +1,21 @@
 from Bowling.score import Score
 
-def test_first_turn_with_7_scores(): 
-    turns = [5,2]
+def test_one_turn_with_7_scores(): 
+    turns = [[5,2]]
     assert(calculate_score(turns) == 7)
 
-def test_second_turn_with_9_scores(): 
-    turns = [6,3]
+def test_one_turn_with_9_scores(): 
+    turns = [[6,3]]
     assert(calculate_score(turns) == 9)
 
+def test_eight_turns_with_nothing_special(): 
+    turns = [[5,2], [6,3], [4,1], [0,6], [5,3], [4,3], [4,4], [2,6]]
+    assert (calculate_score(turns) == 58)
 
-def calculate_score(points): 
-    score = Score(points)
-    return score.Calculate()
+
+def calculate_score(turns): 
+    score = 0
+    for turn in turns : 
+        score += turn[0]
+        score += turn[1]
+    return score
