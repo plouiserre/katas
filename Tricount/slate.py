1e3b638e22b4bf730886a2a7cb357dcd0e61ed2e
plouiserre <plouiserre@gmail.com>
2026-04-17 12:27:18 +0200

    update slate

--- Tricount/slate.py
+++ Tricount/slate.py
@@ -3,24 +3,33 @@ from Tricount.refund import Refund
 class Slate:
     def __init__(self, expenses):
         self.expenses = expenses
+        self.less_expense = 0
+        self.more_expense = 0
+        self.debtor = ""
+        self.refunded_person = ""
+        self.expenses_by_person = {}
+        self.amount_refund = 0
 
     def manage_expenses(self):
-        expenses_by_person ={} 
-        for person in self.expenses: 
-            expenses_by_person[person] = 0
-            for value_expense in self.expenses[person] : 
-                expenses_by_person[person] += value_expense
-        less_expense = 0
-        more_expense = 0
-        debtor = ""
-        refunded_person = ""
-        for person in expenses_by_person: 
-            if expenses_by_person[person] < less_expense or less_expense == 0: 
-                less_expense = expenses_by_person[person]
-                debtor = person
-            if expenses_by_person[person] > more_expense :
-                more_expense = expenses_by_person[person] 
-                refunded_person = person
-        amount_refund = (more_expense - less_expense) / 2
-        refund = Refund(debtor, refunded_person, round(amount_refund, 2))
+        self.__calculate_each_expense_by_friend() 
+        self.__identify_each_element_of_refund()        
+        refund = Refund(self.debtor, self.refunded_person, round(self.amount_refund, 2))
         return refund
+    
+    def __calculate_each_expense_by_friend(self) : 
+        self.expenses_by_person ={} 
+        for person in self.expenses: 
+            self.expenses_by_person[person] = 0
+            for value_expense in self.expenses[person] : 
+                self.expenses_by_person[person] += value_expense
+        return self.expenses_by_person
+    
+    def __identify_each_element_of_refund(self) :
+        for person in self.expenses_by_person: 
+            if self.expenses_by_person[person] < self.less_expense or self.less_expense == 0: 
+                self.less_expense = self.expenses_by_person[person]
+                self.debtor = person
+            if self.expenses_by_person[person] >self. more_expense :
+                self.more_expense = self.expenses_by_person[person] 
+                self.refunded_person = person
+        self.amount_refund = (self.more_expense - self.less_expense) / 2
