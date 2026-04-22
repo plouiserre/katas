a2125e46e2c0b6c5cdaffd280e5601caffc01e04
plouiserre <plouiserre@gmail.com>
2026-04-22 12:32:37 +0200

    first us developed loan calculator without interest

--- LoanCalculator/tests/test_a.py
+++ LoanCalculator/tests/test_a.py
@@ -1,2 +1,13 @@
 def test_1() : 
-    assert(1==2)
+    sum_total = 200000
+    years = 25
+    assert(get_monthly_payment(sum_total, years)==666.67)
+
+def test_2(): 
+    sum_total = 300000
+    years = 20
+    assert(get_monthly_payment(sum_total, years)==1250)
+
+def get_monthly_payment(sum_total, years):
+    montly_payments = round(sum_total / (years*12) ,2) 
+    return montly_payments
