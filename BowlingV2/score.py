5219cb2c779e50f7af8915f226b127e04aa1429e
plouiserre <plouiserre@gmail.com>
2026-04-27 14:51:23 +0200

    link parser with calcul score

--- BowlingV2/score.py
+++ BowlingV2/score.py
@@ -1,12 +1,14 @@
 class Score : 
-    def __init__(self, turns):
-        self.turns = turns
+    def __init__(self, notation, bowling_parser):
+        self.notation = notation
+        self.bowling_parser = bowling_parser
         self.score = 0
         self.is_spare = False 
         self.is_strike = False
 
-    def Calculate(self):        
-        for number_turn, turn in enumerate(self.turns) :    
+    def Calculate(self):     
+        turns = self.bowling_parser.parse_all_bowling_frames(self.notation)   
+        for number_turn, turn in enumerate(turns) :    
             self.__manage_spare_or_strike(turn)        
             self.__determinate_is_spare_or_strike(turn)
             self.score += turn[0]
@@ -29,6 +31,7 @@ class Score :
         elif turn[0]+turn[1] == 10 :            
             self.is_spare = True
 
+    #rewriting this method
     def __manage_last_turn(self, turn):
-        if self.is_spare or self.is_strike: 
-            self.score += turn[2]
+        if self.is_spare or (self.is_strike and len(turn)==3): 
+            self.score += turn[2]        
