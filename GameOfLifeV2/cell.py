81515e5fe656450996b5c709884df090ab97f839
plouiserre <plouiserre@gmail.com>
2026-04-27 11:07:24 +0200

    mv all code for cell evolution in cell

---
+++ GameOfLifeV2/cell.py
@@ -0,0 +1,41 @@
+DEAD = 0
+ALIVE = 1
+
+class Cell : 
+    def __init__(self, x, y, alive_cells, status_cell):
+        self.x = x
+        self.y = y
+        self.alive_cells = alive_cells
+        self.status_cell = status_cell
+
+    def evolve(self): 
+        status_next_generation = ""
+        number_neighbors = self.__count_alive_neighbors([self.y, self.x], self.alive_cells)
+        status_next_generation = self.__get_status_cell_next_round(self.status_cell, number_neighbors)
+        return status_next_generation
+    
+    def __count_alive_neighbors(self, coordonnates, alive_cells): 
+        alive_neighbors = 0
+        for cells in alive_cells : 
+            y = coordonnates[0]
+            x = coordonnates[1]
+            cell_y = cells[0]
+            cell_x = cells[1]
+            for i in range(y - 1, y + 2 ) : 
+                for j in range (x - 1 , x + 2 ) :
+                    same_coordonnates = y == i and x == j
+                    if i == cell_y and j == cell_x  and same_coordonnates == False: 
+                        alive_neighbors +=1  
+        return alive_neighbors
+        
+    def __get_status_cell_next_round(self, actual_status, alive_neighbors): 
+            if actual_status == ALIVE and alive_neighbors < 2 : 
+                return DEAD
+            elif actual_status == ALIVE and alive_neighbors > 3 : 
+                return DEAD
+            elif actual_status == ALIVE and (alive_neighbors == 2 or alive_neighbors == 3): 
+                return ALIVE
+            elif actual_status == DEAD and alive_neighbors == 3 : 
+                return ALIVE
+            else : 
+                return DEAD
--- GameOfLifeV2/tests/test_a.py
+++ GameOfLifeV2/tests/test_a.py
@@ -1,4 +1,4 @@
-from GameOfLifeV2.grid import ALIVE, DEAD
+from GameOfLifeV2.cell import ALIVE, DEAD, Cell
 
 def test_cell_alive_is_dying_because_underpopulation():
     alive_cells = [[0,1],[1,0],[1,1],[2,2]]
@@ -26,33 +26,5 @@ def test_cell_dead_is_alive_because_three_living_cells():
 
 
 def next_state_cell(x, y, alive_cells, status_cell):
-    status_next_generation = ""
-    number_neighbors = __count_alive_neighbors([y, x], alive_cells)
-    status_next_generation = __get_status_cell_next_round(status_cell, number_neighbors)
-    return status_next_generation
-
-def __count_alive_neighbors(coordonnates, alive_cells): 
-        alive_neighbors = 0
-        for cells in alive_cells : 
-            y = coordonnates[0]
-            x = coordonnates[1]
-            cell_y = cells[0]
-            cell_x = cells[1]
-            for i in range(y - 1, y + 2 ) : 
-                for j in range (x - 1 , x + 2 ) :
-                    same_coordonnates = y == i and x == j
-                    if i == cell_y and j == cell_x  and same_coordonnates == False: 
-                        alive_neighbors +=1  
-        return alive_neighbors
-        
-def __get_status_cell_next_round(actual_status, alive_neighbors): 
-        if actual_status == ALIVE and alive_neighbors < 2 : 
-            return DEAD
-        elif actual_status == ALIVE and alive_neighbors > 3 : 
-            return DEAD
-        elif actual_status == ALIVE and (alive_neighbors == 2 or alive_neighbors == 3): 
-            return ALIVE
-        elif actual_status == DEAD and alive_neighbors == 3 : 
-            return ALIVE
-        else : 
-            return DEAD
+    cell = Cell(x, y, alive_cells, status_cell)
+    return cell.evolve()
