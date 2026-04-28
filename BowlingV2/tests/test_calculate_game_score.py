ee35ae6a333b22a5a374032007713ad87f2a3649
plouiserre <plouiserre@gmail.com>
2026-04-28 16:07:58 +0200

    rename test

--- BowlingV2/tests/test_calculate_game_score.py
+++ BowlingV2/tests/test_calculate_game_score.py
@@ -1,31 +1,31 @@
 from BowlingV2.bowling_parser import BowlingParser
 from BowlingV2.score import Score
 
-def test_one_turn_with_7_scores(): 
+def test_calculate_game_score_with_one_frame_with_7_points(): 
     notation = "5 2"    
     assert(calculate_score(notation) == 7)
 
-def test_one_turn_with_9_scores(): 
+def test_calculate_game_score_with_one_frame_with_9_points(): 
     notation = "6 3"
     assert(calculate_score(notation) == 9)
 
-def test_eight_turns_with_nothing_special(): 
+def test_calculate_game_score_with_eight_frames_with_nothing_special(): 
     notation = "5 2|6 3|4 1|- 6|5 3|4 3|4 4|2 6"
     assert (calculate_score(notation) == 58)
 
-def test_eight_turns_with_spare(): 
+def test_calculate_game_score_with_eight_frames_with_one_spare(): 
     notation = "5 2|6 3|4 1|- 6|5 3|4 3|4 /|2 6"
     assert (calculate_score(notation) == 62)
 
-def test_eight_turns_with_strike(): 
+def test_calculate_game_score_with_eight_frames_with_one_strike(): 
     notation = "5 2|6 3|4 1|- 6|5 3|4 3|X |2 6"
     assert (calculate_score(notation) == 68)
 
-def test_ten_turns_spare(): 
+def test_calculate_all_game_score_finishing_by_a_spare(): 
     notation = "5 2|6 3|4 1|- 6|5 3|4 3|4 4|2 6|1 5|8 / 6"
     assert (calculate_score(notation) == 80)
 
-def test_ten_turns_strike(): 
+def test_calculate_all_game_score_finishing_by_a_strike(): 
     notation = "5 2|6 3|4 1|- 6|5 3|4 3|4 4|2 6|1 5|X 6"
     assert (calculate_score(notation) == 80)
 
