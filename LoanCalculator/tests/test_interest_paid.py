667e353facc0aa7308e518440ca89665b108d9bc
plouiserre <plouiserre@gmail.com>
2026-05-07 11:57:24 +0200

    interest paid clean

--- LoanCalculator/tests/test_interest_paid.py
+++ LoanCalculator/tests/test_interest_paid.py
@@ -1,12 +1,14 @@
-def test_1(): 
+from LoanCalculator.interest_paid import InterestPaid
+def test_calculate_interest_to_paid_with_1000000_capital_and_two_point_three_rate(): 
     capital = 100000
     interest_by_month = 2.3/100/12
     assert(191.67 == calculate_interest_to_paid(capital, interest_by_month))
 
-def test_2(): 
+def test_calculate_interest_to_paid_with_3000000_capital_and_three_point_nine_rate(): 
     capital = 300000
     interest_by_month = 3.9/100/12
     assert(975 == calculate_interest_to_paid(capital, interest_by_month))
 
 def calculate_interest_to_paid(capital, interest_by_month): 
-    return round(capital * interest_by_month, 2)
+    interest_paid = InterestPaid(capital, interest_by_month)
+    return interest_paid.calculate()
