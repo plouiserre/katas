16d972875b5cfe95a8cf97be3d094044433d1963
plouiserre <plouiserre@gmail.com>
2026-05-07 16:41:21 +0200

    capital remaining notion developed

--- LoanCalculator/tests/test_capital_repaid.py
+++ LoanCalculator/tests/test_capital_repaid.py
@@ -4,8 +4,8 @@ def test_calculate_capital_remain():
     montly_payment = 988
     interests = 62.6
     capital_repaid_expected = 925.4
-    assert(capital_repaid_expected == assert_capital_repaid(montly_payment, interests))
+    assert(capital_repaid_expected == calculate_capital_repaid(montly_payment, interests))
 
-def assert_capital_repaid(montly_payment, interests):
+def calculate_capital_repaid(montly_payment, interests):
     capital_repaid = Capital_Repaid(montly_payment, interests) 
     return capital_repaid.calculate_amount()
