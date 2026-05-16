23650b42c3de241164edc104adb206e39c408a74
plouiserre <plouiserre@gmail.com>
2026-05-16 16:13:04 +0200

    finish calculate taeg

--- LoanCalculator/tests/test_taeg.py
+++ LoanCalculator/tests/test_taeg.py
@@ -1,6 +1,6 @@
 from LoanCalculator.taeg import Taeg
 
-def test_1():
+def test_calculate_taeg_capital_100000_10_years_933_point_63_mensuality_cost_guarantee_41_point_67_application_fee_800():
     capital = 100000
     montly_paiment = 933.63
     years = 10
@@ -8,7 +8,7 @@ def test_1():
     application_fee = 800
     assert(3.35 == calculate_taeg(capital, years, montly_paiment, cost_guarantee, application_fee))
 
-def test_2():
+def test_calculate_taeg_capital_300000_25_years_1566_point_99_mensuality_cost_guarantee_100application_fee_3600():
     capital = 300000
     montly_paiment = 1566.99
     years = 25
