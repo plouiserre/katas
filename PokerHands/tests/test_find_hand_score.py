562b181247b982d5f127e83b72631272f6ebfe40
plouiserre <plouiserre@gmail.com>
2026-05-20 15:29:16 +0200

    try to identify three of kind in a poker hand

--- PokerHands/tests/test_find_hand_score.py
+++ PokerHands/tests/test_find_hand_score.py
@@ -86,37 +86,116 @@ def test_find_pair_ace():
     cards =  [Card(CardValue.ACE, CardColor.CLUBS), Card(CardValue.ACE, CardColor.DIAMONDS), Card(CardValue.SEVEN, CardColor.HEARTS), Card(CardValue.SIX, CardColor.SPADES), Card(CardValue.FOUR, CardColor.SPADES)]
     assert(return_high_hands(cards)==Score(HighFigure.PAIR, FigureValue.ACE_SCORE))
 
+def test_find_three_of_a_two(): 
+    cards =  [Card(CardValue.TWO, CardColor.CLUBS), Card(CardValue.ACE, CardColor.DIAMONDS), Card(CardValue.TWO, CardColor.HEARTS), Card(CardValue.TWO, CardColor.SPADES), Card(CardValue.FOUR, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.TWO_SCORE))
+
+def test_find_three_of_a_three(): 
+    cards =  [Card(CardValue.THREE, CardColor.CLUBS), Card(CardValue.THREE, CardColor.DIAMONDS), Card(CardValue.THREE, CardColor.HEARTS), Card(CardValue.TWO, CardColor.SPADES), Card(CardValue.FOUR, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.THREE_SCORE))
+
+def test_find_three_of_a_four(): 
+    cards =  [Card(CardValue.FOUR, CardColor.CLUBS), Card(CardValue.FOUR, CardColor.DIAMONDS), Card(CardValue.THREE, CardColor.HEARTS), Card(CardValue.TWO, CardColor.SPADES), Card(CardValue.FOUR, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.FOUR_SCORE))
+
+def test_find_three_of_a_five(): 
+    cards =  [Card(CardValue.FIVE, CardColor.CLUBS), Card(CardValue.FIVE, CardColor.DIAMONDS), Card(CardValue.FIVE, CardColor.HEARTS), Card(CardValue.TWO, CardColor.SPADES), Card(CardValue.FOUR, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.FIVE_SCORE))
+
+def test_find_three_of_a_six(): 
+    cards =  [Card(CardValue.SIX, CardColor.CLUBS), Card(CardValue.SIX, CardColor.DIAMONDS), Card(CardValue.SIX, CardColor.HEARTS), Card(CardValue.TWO, CardColor.SPADES), Card(CardValue.FOUR, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.SIX_SCORE))
+
+def test_find_three_of_a_seven(): 
+    cards =  [Card(CardValue.SEVEN, CardColor.CLUBS), Card(CardValue.SIX, CardColor.DIAMONDS), Card(CardValue.SEVEN, CardColor.HEARTS), Card(CardValue.TWO, CardColor.SPADES), Card(CardValue.SEVEN, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.SEVEN_SCORE))
+
+def test_find_three_of_a_eight(): 
+    cards =  [Card(CardValue.EIGHT, CardColor.CLUBS), Card(CardValue.EIGHT, CardColor.DIAMONDS), Card(CardValue.SEVEN, CardColor.HEARTS), Card(CardValue.TWO, CardColor.SPADES), Card(CardValue.EIGHT, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.EIGHT_SCORE))
+
+def test_find_three_of_a_nine(): 
+    cards =  [Card(CardValue.NINE, CardColor.CLUBS), Card(CardValue.EIGHT, CardColor.DIAMONDS), Card(CardValue.SEVEN, CardColor.HEARTS), Card(CardValue.NINE, CardColor.SPADES), Card(CardValue.NINE, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.NINE_SCORE))
+
+def test_find_three_of_a_ten(): 
+    cards =  [Card(CardValue.TEN, CardColor.CLUBS), Card(CardValue.TEN, CardColor.DIAMONDS), Card(CardValue.TEN, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.SPADES), Card(CardValue.NINE, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.TEN_SCORE))
+
+def test_find_three_of_a_jack(): 
+    cards =  [Card(CardValue.JACK, CardColor.CLUBS), Card(CardValue.JACK, CardColor.DIAMONDS), Card(CardValue.JACK, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.SPADES), Card(CardValue.NINE, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.JACK_SCORE))
+
+def test_find_three_of_a_queen(): 
+    cards =  [Card(CardValue.QUEEN, CardColor.CLUBS), Card(CardValue.JACK, CardColor.DIAMONDS), Card(CardValue.QUEEN, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.SPADES), Card(CardValue.NINE, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.QUEEN_SCORE))
+
+def test_find_three_of_a_king(): 
+    cards =  [Card(CardValue.KING, CardColor.CLUBS), Card(CardValue.JACK, CardColor.DIAMONDS), Card(CardValue.KING, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.SPADES), Card(CardValue.KING, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.KING_SCORE))
+
+def test_find_three_of_a_ace(): 
+    cards =  [Card(CardValue.ACE, CardColor.CLUBS), Card(CardValue.ACE, CardColor.DIAMONDS), Card(CardValue.ACE, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.SPADES), Card(CardValue.KING, CardColor.SPADES)]
+    assert(return_high_hands(cards)==Score(HighFigure.THREE_OF_A_KIND, FigureValue.ACE_SCORE))
+
 def return_high_hands(cards):
     hand = Hand(cards)
     cards_identified = hand.count_all_cards()
     if CardValue.TWO in cards_identified and cards_identified[CardValue.TWO] == 2 : 
         return Score(HighFigure.PAIR, FigureValue.TWO_SCORE)
-    elif CardValue.THREE in cards_identified and cards_identified[CardValue.THREE] == 2 == 2 : 
+    elif CardValue.THREE in cards_identified and cards_identified[CardValue.THREE] == 2: 
         return Score(HighFigure.PAIR, FigureValue.THREE_SCORE)
-    elif CardValue.FOUR in cards_identified and cards_identified[CardValue.FOUR] == 2 == 2 : 
+    elif CardValue.FOUR in cards_identified and cards_identified[CardValue.FOUR] == 2: 
         return Score(HighFigure.PAIR, FigureValue.FOUR_SCORE)
-    elif CardValue.FIVE in cards_identified and cards_identified[CardValue.FIVE] == 2 == 2 : 
+    elif CardValue.FIVE in cards_identified and cards_identified[CardValue.FIVE] == 2: 
         return Score(HighFigure.PAIR, FigureValue.FIVE_SCORE)
-    elif CardValue.SIX in cards_identified and cards_identified[CardValue.SIX] == 2 == 2 : 
+    elif CardValue.SIX in cards_identified and cards_identified[CardValue.SIX] == 2: 
         return Score(HighFigure.PAIR, FigureValue.SIX_SCORE)
-    elif CardValue.SEVEN in cards_identified and cards_identified[CardValue.SEVEN] == 2 == 2 : 
+    elif CardValue.SEVEN in cards_identified and cards_identified[CardValue.SEVEN] == 2: 
         return Score(HighFigure.PAIR, FigureValue.SEVEN_SCORE)
-    elif CardValue.EIGHT in cards_identified and cards_identified[CardValue.EIGHT] == 2 == 2 :
+    elif CardValue.EIGHT in cards_identified and cards_identified[CardValue.EIGHT] == 2:
         return Score(HighFigure.PAIR, FigureValue.EIGHT_SCORE)
-    elif CardValue.NINE in cards_identified and cards_identified[CardValue.NINE] == 2 == 2 :
+    elif CardValue.NINE in cards_identified and cards_identified[CardValue.NINE] == 2:
         return Score(HighFigure.PAIR, FigureValue.NINE_SCORE)
-    elif CardValue.TEN in cards_identified and cards_identified[CardValue.TEN] == 2 == 2 :
+    elif CardValue.TEN in cards_identified and cards_identified[CardValue.TEN] == 2:
         return Score(HighFigure.PAIR, FigureValue.TEN_SCORE)
-    elif CardValue.JACK in cards_identified and cards_identified[CardValue.JACK] == 2 == 2 :
+    elif CardValue.JACK in cards_identified and cards_identified[CardValue.JACK] == 2:
         return Score(HighFigure.PAIR, FigureValue.JACK_SCORE)
-    elif CardValue.QUEEN in cards_identified and cards_identified[CardValue.QUEEN] == 2 == 2 :
+    elif CardValue.QUEEN in cards_identified and cards_identified[CardValue.QUEEN] == 2:
         return Score(HighFigure.PAIR, FigureValue.QUEEN_SCORE)
-    elif CardValue.KING in cards_identified and cards_identified[CardValue.KING] == 2 == 2 :
+    elif CardValue.KING in cards_identified and cards_identified[CardValue.KING] == 2 :
         return Score(HighFigure.PAIR, FigureValue.KING_SCORE)
-    elif CardValue.ACE in cards_identified and cards_identified[CardValue.ACE] == 2 == 2 :
+    elif CardValue.ACE in cards_identified and cards_identified[CardValue.ACE] == 2:
         return Score(HighFigure.PAIR, FigureValue.ACE_SCORE)
+    elif CardValue.TWO in cards_identified and cards_identified[CardValue.TWO] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.TWO_SCORE)
+    elif CardValue.THREE in cards_identified and cards_identified[CardValue.THREE] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.THREE_SCORE)
+    elif CardValue.FOUR in cards_identified and cards_identified[CardValue.FOUR] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.FOUR_SCORE)
+    elif CardValue.FIVE in cards_identified and cards_identified[CardValue.FIVE] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.FIVE_SCORE)
+    elif CardValue.SIX in cards_identified and cards_identified[CardValue.SIX] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.SIX_SCORE)
+    elif CardValue.SEVEN in cards_identified and cards_identified[CardValue.SEVEN] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.SEVEN_SCORE)
+    elif CardValue.EIGHT in cards_identified and cards_identified[CardValue.EIGHT] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.EIGHT_SCORE)
+    elif CardValue.NINE in cards_identified and cards_identified[CardValue.NINE] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.NINE_SCORE)
+    elif CardValue.TEN in cards_identified and cards_identified[CardValue.TEN] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.TEN_SCORE)
+    elif CardValue.JACK in cards_identified and cards_identified[CardValue.JACK] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.JACK_SCORE)
+    elif CardValue.QUEEN in cards_identified and cards_identified[CardValue.QUEEN] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.QUEEN_SCORE)
+    elif CardValue.KING in cards_identified and cards_identified[CardValue.KING] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.KING_SCORE)
+    elif CardValue.ACE in cards_identified and cards_identified[CardValue.ACE] == 3:
+        return Score(HighFigure.THREE_OF_A_KIND, FigureValue.ACE_SCORE)
     else : 
         return __manage_high_score_Score(cards)
+    
     
 def __manage_high_score_Score(cards):
     max_value = FigureValue.TWO_SCORE
