d521300acc385f3731be4188266fcf182fd9fda9
plouiserre <plouiserre@gmail.com>
2026-06-28 16:36:45 +0200

    combinatoires tests pairs developed

--- PokerHands/tests/detector/test_find_pair_detector.py
+++ PokerHands/tests/detector/test_find_pair_detector.py
@@ -65,18 +65,68 @@ def test_find_pair_ace():
 
 def test_find_pair_random(): 
     all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    pair_value = __random_creation_pairs(all_cards_values)
-    high_value = __find_high_card_value(all_cards_values, [pair_value])
-    forbidden_value = [pair_value, high_value]
-    first_value = __find_other_cards(all_cards_values, forbidden_value, high_value)
-    second_value = __find_other_cards(all_cards_values, forbidden_value, high_value)
-    all_values_accepted = [pair_value, first_value, second_value, pair_value, high_value]
-    hand = []
-    while len(hand)  < 5 :
-        index = random.randint(0, len(all_values_accepted) - 1)
-        value_choose = all_values_accepted[index]
-        hand.append(value_choose)
-        all_values_accepted.remove(value_choose)
+    pair_value = __get_pair_value(all_cards_values)
+    high_card = __get_high_cards_value(all_cards_values, pair_value)
+    print(pair_value)
+    print(high_card)
+    # other_cards = __get_others_cards(all_cards_values, CardValue.FOUR, CardValue.FIVE)
+    other_cards = __get_others_cards(all_cards_values, pair_value, high_card)
+    hands = []
+    hands.append(pair_value)
+    hands.append(pair_value)
+    hands.append(high_card)
+    hands.extend(other_cards)
+    random.shuffle(hands)
+    assert(_find_pair(hands)==PairFigure(pair_value, high_card))
+    
+def __get_pair_value(all_cards_values):
+    index = random.randint(0, len(all_cards_values) - 1)
+    pair_value = all_cards_values[index]
+    return pair_value
+
+def __get_high_cards_value(all_cards_values, pair_value): 
+    all_cards_values_for_high_cards = copy.deepcopy(all_cards_values)
+    all_cards_values_for_high_cards.remove(pair_value)
+    if CardValue.TWO in all_cards_values_for_high_cards :
+        all_cards_values_for_high_cards.remove(CardValue.TWO)
+    if CardValue.THREE in all_cards_values_for_high_cards : 
+        all_cards_values_for_high_cards.remove(CardValue.THREE)    
+    if CardValue.FOUR in all_cards_values_for_high_cards :
+        all_cards_values_for_high_cards.remove(CardValue.FOUR)
+    if pair_value == CardValue.TWO or pair_value == CardValue.THREE or pair_value == CardValue.FOUR : 
+        all_cards_values_for_high_cards.remove(CardValue.FIVE)
+    index = random.randint(0, len(all_cards_values_for_high_cards) - 1)
+    high_cards_value = all_cards_values_for_high_cards[index]
+    return high_cards_value
+
+def __get_others_cards(all_cards_values, pair_value, high_card) : 
+    copy_all_cards_values = copy.deepcopy(all_cards_values)
+    for card_value in copy_all_cards_values : 
+        if high_card <= card_value : 
+            all_cards_values.remove(card_value)
+    if pair_value in all_cards_values : 
+        all_cards_values.remove(pair_value)
+    others_cards = []
+    while len(others_cards) < 3 :
+        index = random.randint(0, len(all_cards_values) - 1)
+        other_card_value = all_cards_values[index]
+        others_cards.append(other_card_value)
+        all_cards_values.remove(other_card_value)
+    return others_cards
+
+
+    # pair_value = __random_creation_pairs(all_cards_values)
+    # high_value = __find_high_card_value(all_cards_values, [pair_value])
+    # forbidden_value = [pair_value, high_value]
+    # first_value = __find_other_cards(all_cards_values, forbidden_value, high_value)
+    # second_value = __find_other_cards(all_cards_values, forbidden_value, high_value)
+    # all_values_accepted = [pair_value, first_value, second_value, pair_value, high_value]
+    # hand = []
+    # while len(hand)  < 5 :
+    #     index = random.randint(0, len(all_values_accepted) - 1)
+    #     value_choose = all_values_accepted[index]
+    #     hand.append(value_choose)
+    #     all_values_accepted.remove(value_choose)
     assert(_find_pair(hand)==PairFigure(pair_value, high_value))
 
     
@@ -86,31 +136,31 @@ def _find_pair(hand):
     pair_detector = PairDetector(counting_cards)
     return pair_detector.find_pair(hand)
 
-def __random_creation_pairs(all_card_value):
-    index = random.randint(0, len(all_card_value) - 1) 
-    pair_cards = all_card_value[index]
-    return pair_cards
+# def __random_creation_pairs(all_card_value):
+#     index = random.randint(0, len(all_card_value) - 1) 
+#     pair_cards = all_card_value[index]
+#     return pair_cards
 
-def __find_high_card_value(all_card_value, card_values_forbidden):
-    all_values_possibles = []
-    for card_value in all_card_value : 
-        if card_value > CardValue.THREE : 
-            all_values_possibles.append(card_value)
-    for value_possible in all_values_possibles : 
-        for card_value_forbidden in card_values_forbidden : 
-            if value_possible == card_value_forbidden : 
-                all_values_possibles.remove(value_possible)
-    index = random.randint(0, len(all_values_possibles) - 1)
-    value_choose = all_values_possibles[index]
-    return value_choose
+# def __find_high_card_value(all_card_value, card_values_forbidden):
+#     all_values_possibles = []
+#     for card_value in all_card_value : 
+#         if card_value > CardValue.THREE : 
+#             all_values_possibles.append(card_value)
+#     for value_possible in all_values_possibles : 
+#         for card_value_forbidden in card_values_forbidden : 
+#             if value_possible == card_value_forbidden : 
+#                 all_values_possibles.remove(value_possible)
+#     index = random.randint(0, len(all_values_possibles) - 1)
+#     value_choose = all_values_possibles[index]
+#     return value_choose
 
-def __find_other_cards(all_card_value, card_values_forbidden, high_card_value) : 
-    all_values_possibles = copy.deepcopy(all_card_value)
-    for value_possible in all_values_possibles : 
-        for card_value_forbidden in card_values_forbidden : 
-            if value_possible == card_value_forbidden or  value_possible > high_card_value  : 
-                all_values_possibles.remove(value_possible)
-    index = random.randint(0, len(all_values_possibles) - 1)
-    value_choose = all_values_possibles[index]
-    card_values_forbidden.append(value_choose)
-    return value_choose
+# def __find_other_cards(all_card_value, card_values_forbidden, high_card_value) : 
+#     all_values_possibles = copy.deepcopy(all_card_value)
+#     for value_possible in all_values_possibles : 
+#         for card_value_forbidden in card_values_forbidden : 
+#             if value_possible == card_value_forbidden or  value_possible > high_card_value  : 
+#                 all_values_possibles.remove(value_possible)
+#     index = random.randint(0, len(all_values_possibles) - 1)
+#     value_choose = all_values_possibles[index]
+#     card_values_forbidden.append(value_choose)
+#     return value_choose
