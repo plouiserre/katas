e05e774ce79353ce2e7a6722d7802d96ca57f46d
plouiserre <plouiserre@gmail.com>
2026-06-28 16:58:11 +0200

    two pairs tests randomized

--- PokerHands/tests/detector/test_find_two_pairs_detector.py
+++ PokerHands/tests/detector/test_find_two_pairs_detector.py
@@ -1,3 +1,5 @@
+import random
+
 from PokerHands.card import Card, CardColor, CardValue
 from PokerHands.counting_cards import CountingCards
 from PokerHands.AllFigures.TwoPairFigure import TwoPairFigure
@@ -30,6 +32,32 @@ def test_find_two_pairs_jack_queen():
 def test_find_two_pairs_king_six(): 
     hand =  [Card(CardValue.QUEEN, CardColor.CLUBS), Card(CardValue.SIX, CardColor.DIAMONDS), Card(CardValue.SIX, CardColor.HEARTS), Card(CardValue.KING, CardColor.SPADES), Card(CardValue.KING, CardColor.SPADES)]
     assert(__find_two_pairs(hand)==TwoPairFigure(CardValue.KING, CardValue.SIX, CardValue.QUEEN))
+
+def test_find_two_pairs_randomise(): 
+    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
+    first_pair_value = __get_card_value_random(all_cards_values)
+    all_cards_values.remove(first_pair_value)
+    second_pair_value = __get_card_value_random(all_cards_values)
+    all_cards_values.remove(second_pair_value)
+    high_card_value = __get_card_value_random(all_cards_values)
+    hand = __get_hand_shuffle(first_pair_value, second_pair_value, high_card_value)
+    assert(__find_two_pairs(hand)==TwoPairFigure(first_pair_value, second_pair_value, high_card_value) or __find_two_pairs(hand)==TwoPairFigure(second_pair_value, first_pair_value, high_card_value))
+
+
+def __get_hand_shuffle(first_pair_value, second_pair_value, high_card_value):
+    hands = []
+    hands.append(first_pair_value)
+    hands.append(first_pair_value)
+    hands.append(second_pair_value)
+    hands.append(second_pair_value)
+    hands.append(high_card_value)
+    random.shuffle(hands)
+    return hands
+
+def __get_card_value_random(all_cards_values):
+    index = random.randint(0, len(all_cards_values) - 1)
+    pair_value = all_cards_values[index]
+    return pair_value
 
 def __find_two_pairs(hand):
     counting_cards = CountingCards()
