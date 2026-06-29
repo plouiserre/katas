ebaa8e44bdc06411c27041fa0d11d65e0494c70f
plouiserre <plouiserre@gmail.com>
2026-06-29 16:55:13 +0200

    randomize tests v5

--- PokerHands/tests/detector/test_find_high_card.py
+++ PokerHands/tests/detector/test_find_high_card.py
@@ -1,89 +1,46 @@
 import random
 import copy
+from typing import List
 from PokerHands.card import Card, CardColor, CardValue
 from PokerHands.detector.high_card_detector import HighCardDetector
 from PokerHands.AllFigures.HighCardFigure import HighCardFigure
 
-def test_find_high_card_king():
-    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    values_already_choose = []
-    forbidden_values = [CardValue.KING, CardValue.ACE]
-    hand =  [Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.CLUBS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS),
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.HEARTS), 
-             Card(CardValue.KING, CardColor.SPADES)]
-    assert(__find_high_card(hand)== HighCardFigure(CardValue.KING))
-
 def test_find_high_card_queen():
-    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    values_already_choose = []
-    forbidden_values = [CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    hand =  [Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.CLUBS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS),
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS), Card(CardValue.QUEEN, CardColor.SPADES), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.HEARTS)]
+    hand =  [Card(CardValue.TWO, CardColor.CLUBS), Card(CardValue.THREE, CardColor.DIAMONDS),Card(CardValue.NINE, CardColor.DIAMONDS), Card(CardValue.TEN, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.SPADES)]
     assert(__find_high_card(hand)==HighCardFigure(CardValue.QUEEN))
 
 def test_find_high_card_jack():
-    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    values_already_choose = []
-    forbidden_values = [CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    print(values_already_choose)
-    hand =  [Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.CLUBS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS),
-             Card(CardValue.JACK, CardColor.DIAMONDS), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.HEARTS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.SPADES)]
+    hand =  [Card(CardValue.TWO, CardColor.CLUBS), Card(CardValue.THREE, CardColor.DIAMONDS),Card(CardValue.NINE, CardColor.DIAMONDS), Card(CardValue.TEN, CardColor.HEARTS), Card(CardValue.JACK, CardColor.SPADES)]
     assert(__find_high_card(hand)==HighCardFigure(CardValue.JACK))
 
-def test_find_high_card_ten():
-    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    values_already_choose = []
-    forbidden_values = [CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    hand =  [Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.CLUBS), Card(CardValue.TEN, CardColor.DIAMONDS), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.HEARTS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.SPADES), 
-             Card(CardValue.SEVEN, CardColor.SPADES)]
-    assert(__find_high_card(hand)==HighCardFigure(CardValue.TEN))
 
-def test_find_high_card_nine():
+def test_find_high_card_random(): 
+    hand : List[Card]= []
+    high_card_random = __get_high_card_value()
+    hand.append(Card(high_card_random, CardColor.CLUBS))
+    all_colors = [CardColor.CLUBS, CardColor.DIAMONDS, CardColor.HEARTS, CardColor.SPADES]
     all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    values_already_choose = []
-    forbidden_values = [CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    hand =  [Card(CardValue.NINE, CardColor.CLUBS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.HEARTS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.SPADES), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.SPADES)]
-    assert(__find_high_card(hand)==HighCardFigure(CardValue.NINE))
+    all_cards_values.remove(high_card_random)
+    all_cards_values_copy = copy.deepcopy(all_cards_values)
+    for card_value in all_cards_values_copy : 
+        if high_card_random <= card_value :
+            all_cards_values.remove(card_value)
+    while len(hand) < 5 : 
+        index_value = random.randint(0, len(all_cards_values) - 1)
+        index_color = random.randint(0, len(all_colors) - 1)
+        value = all_cards_values[index_value]
+        color = all_colors[index_color]
+        hand.append(Card(value, color))
+        all_cards_values.remove(value)
+    assert(__find_high_card(hand)==HighCardFigure(high_card_random))
+    
 
-def test_find_high_card_eight():
-    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    values_already_choose = []
-    forbidden_values = [CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    hand =  [Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.CLUBS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.HEARTS), Card(CardValue.EIGHT, CardColor.SPADES), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.SPADES)]
-    assert(__find_high_card(hand)==HighCardFigure(CardValue.EIGHT))
-
-def test_find_high_card_seven():
-    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR, CardValue.FIVE, CardValue.SIX, CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    values_already_choose = []
-    forbidden_values = [CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
-    hand =  [Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.CLUBS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.DIAMONDS), 
-             Card(CardValue.SEVEN, CardColor.HEARTS), Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.SPADES), 
-             Card(__choose_card_value(all_cards_values, values_already_choose, values_already_choose, forbidden_values), CardColor.SPADES)]
-    assert(__find_high_card(hand)==HighCardFigure(CardValue.SEVEN))
+def __get_high_card_value():
+    all_cards_values = [CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]
+    index = random.randint(0, len(all_cards_values) - 1)
+    pair_value = all_cards_values[index]
+    return pair_value
 
 def __find_high_card(hand):
     _high_card_detector = HighCardDetector()
     return _high_card_detector.find_high_card(hand)
-
-def __choose_card_value(all_cards_values, values_already_choose, already_choose_values, forbidden_values): 
-    not_choose_values = copy.deepcopy(all_cards_values)
-    for card_value in all_cards_values : 
-        for already_choose_value in already_choose_values : 
-            if card_value == already_choose_value : 
-                not_choose_values.remove(card_value)
-    ok_values = copy.deepcopy(not_choose_values)
-    for not_choose_value in not_choose_values : 
-        for forbidden_value in forbidden_values : 
-            if not_choose_value == forbidden_value : 
-                ok_values.remove(forbidden_value)
-    index = random.randint(0, len(ok_values) - 1)
-    value_choose = ok_values[index]
-    values_already_choose.append(value_choose)
-    return value_choose
--- PokerHands/tests/figure/test_compare_flush_hands.py
+++ PokerHands/tests/figure/test_compare_flush_hands.py
@@ -1,6 +1,9 @@
+import copy
+import random 
+
 from PokerHands.card import CardValue, CardColor
 from PokerHands.AllFigures.FlushFigure import FlushFigure 
-from PokerHands.score_tmp import FIRST_HAND, EQUALITY
+from PokerHands.score_tmp import FIRST_HAND, SECOND_HAND, EQUALITY
 
 def test_compare_where_each_hand_have_differents_flush(): 
     first_hand = FlushFigure(CardColor.SPADES, CardValue.EIGHT)
@@ -11,6 +14,61 @@ def test_compare_where_each_hand_have_same_flush():
     first_hand = FlushFigure(CardColor.HEARTS, CardValue.ACE)
     second_hand = FlushFigure(CardColor.DIAMONDS, CardValue.ACE)
     assert(EQUALITY == compare_two_flush_hands(first_hand, second_hand))
+
+def test_compare_randomise_flush_with_first_hand_win(): 
+    high_card  = __get_high_card_value()
+    all_colors = [CardColor.CLUBS, CardColor.DIAMONDS, CardColor.HEARTS, CardColor.SPADES]
+    lower_card_value = __get_lower_card_value(high_card)
+    high_card_color = __get_colors_random(all_colors)
+    lower_card_color = __get_colors_random(all_colors)
+    first_hand = FlushFigure(high_card_color, high_card)
+    second_hand = FlushFigure(lower_card_color, lower_card_value)
+    assert(FIRST_HAND == compare_two_flush_hands(first_hand, second_hand))
+
+def test_compare_randomise_flush_with_second_hand_win(): 
+    high_card  = __get_high_card_value()
+    print(high_card)
+    all_colors = [CardColor.CLUBS, CardColor.DIAMONDS, CardColor.HEARTS, CardColor.SPADES]
+    lower_card_value = __get_lower_card_value(high_card)
+    print(lower_card_value)
+    high_card_color = __get_colors_random(all_colors)
+    lower_card_color = __get_colors_random(all_colors)
+    first_hand = FlushFigure(lower_card_color, lower_card_value) 
+    second_hand = FlushFigure(high_card_color, high_card)
+    assert(SECOND_HAND == compare_two_flush_hands(first_hand, second_hand))
+
+def test_compare_randomise_flush_with_equality(): 
+    card_value  = __get_high_card_value()
+    all_colors = [CardColor.CLUBS, CardColor.DIAMONDS, CardColor.HEARTS, CardColor.SPADES]
+    high_card_color = __get_colors_random(all_colors)
+    lower_card_color = __get_colors_random(all_colors)
+    first_hand = FlushFigure(lower_card_color, card_value) 
+    second_hand = FlushFigure(high_card_color, card_value)
+    assert(EQUALITY == compare_two_flush_hands(first_hand, second_hand))
+
+def __get_high_card_value():
+    all_cards_values = [CardValue.THREE, CardValue.FOUR,CardValue.FIVE,CardValue.SIX,
+                        CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]    
+    index = random.randint(0, len(all_cards_values) - 1)
+    pair_value = all_cards_values[index]
+    return pair_value
+
+def __get_lower_card_value(high_card_value):
+    all_cards_values = [CardValue.TWO, CardValue.THREE, CardValue.FOUR,CardValue.FIVE,CardValue.SIX,
+                        CardValue.SEVEN, CardValue.EIGHT, CardValue.NINE, CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING, CardValue.ACE]    
+    all_cards_values_copy = copy.deepcopy(all_cards_values)
+    for card_value in all_cards_values_copy : 
+        if high_card_value <= card_value : 
+            all_cards_values.remove(card_value)
+    index_lower_card = random.randint(0, len(all_cards_values) - 1)
+    lower_card_value = all_cards_values[index_lower_card]
+    return lower_card_value
+
+def __get_colors_random(all_colors):
+    index_card_color = random.randint(0, len(all_colors) - 1)
+    card_color = all_colors[index_card_color]
+    all_colors.remove(card_color)
+    return card_color
 
 def compare_two_flush_hands(first_hand, second_hand): 
     result = first_hand.compare_with_other_flush_hands(second_hand)
