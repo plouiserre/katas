2da5dcb0e1bb61d83d0c8d8723bef2ec1df1235c
plouiserre <plouiserre@gmail.com>
2026-07-05 15:49:08 +0200

    rename tests

--- GameOfLife/tests/test_game.py
+++ GameOfLife/tests/test_game.py
@@ -4,19 +4,19 @@ simple_grid = [".*.", "**.", "..."]
 medium_grid = ["*.*", ".*.", "..*"]
 complexe_grid = ["...**...", "..*.*...", "...**...", ".*......"]
 
-def test_1():
+def test_throw_game_with_simple_grid_three_turns():
     grid_evoluting  = __get_all_turns(simple_grid, 3)
     assert(grid_evoluting[0] == [".*.", "**.", "..."])
     assert(grid_evoluting[1] == ["**.", "**.", "..."])
     assert(grid_evoluting[2] == ["**.", "**.", "..."])
 
-def test_2(): #il y a un soucis dans cette ligne!!!!!
+def test_throw_game_with_medium_grid_three_turns(): #il y a un soucis dans cette ligne!!!!!
     grid_evoluting = __get_all_turns(medium_grid, 3)
     assert(grid_evoluting[0] == ["*.*", ".*.", "..*"])
     assert(grid_evoluting[1] == [".*.", ".**", "..."])
     assert(grid_evoluting[2] == [".**", ".**", "..."])
 
-def test_3(): 
+def test_throw_game_with_complexe_grid_three_turns(): 
     grid_evoluting = __get_all_turns(complexe_grid, 3)
     assert(grid_evoluting[0] == ["...**...", "..*.*...", "...**...", ".*......"])
     assert(grid_evoluting[1] == ["...**...", "..*..*..", "..***...","........"])
