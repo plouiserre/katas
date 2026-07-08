864559be3e5a1e272d2d67ef5d39c5640b306545
plouiserre <plouiserre@gmail.com>
2026-07-08 13:51:33 +0200

    rename all tests

--- PokerHands/tests/test_deck.py
+++ PokerHands/tests/test_deck.py
@@ -1,12 +1,12 @@
 from PokerHands.deck import Deck
 
-def test_1() : 
+def test_check_in_one_time_you_drawn_randownly_five_cards_uniques() : 
     all_cards_drawned = drawn_randomly_five_cards()
     
     assert(len(all_cards_drawned) == 5)
     assert(len(all_cards_drawned) == len(set(all_cards_drawned)))
 
-def test_2() : 
+def test_check_in_one_tousand_time_you_drawn_randownly_five_cards_uniques() : 
     attempt = 0
     while attempt < 5000 :
         all_cards_drawned = drawn_randomly_five_cards()
