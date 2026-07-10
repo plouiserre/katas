06a6368403d5bd649afd5c45f1c8b2fa1685bc83
plouiserre <plouiserre@gmail.com>
2026-07-10 14:37:35 +0200

    create main.py

--- PokerHands/player.py
+++ PokerHands/player.py
@@ -3,12 +3,13 @@ from PokerHands.card import Card
 class Player : 
     def __init__(self, hand):
         self.hand = hand
+        self.hand_content = None
 
     def calculate_hand(self, hand_content):
+        self.hand_content = hand_content
         all_cards = []
-        for card_acronym in hand_content :
+        for card_acronym in self.hand_content :
             new_card = Card.parse(card_acronym)
             all_cards.append(new_card)
         high_figure = self.hand.determinate_high_figure(all_cards)
         return high_figure
-    
