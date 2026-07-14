27380d91fefc12a45d2dfe942244576b98e2e1b1
plouiserre <plouiserre@gmail.com>
2026-07-14 18:54:58 +0200

    rename tests with good contexts

--- BirthdayGreetings/tests/test_wishing_birthday.py
+++ BirthdayGreetings/tests/test_wishing_birthday.py
@@ -3,7 +3,7 @@ from BirthdayGreetings.Contact import Contact
 from BirthdayGreetings.Template import Template
 from BirthdayGreetings.WishingBirthday import WishingBirthday
 
-def test_1():
+def test_wishing_birthday_for_three_interstellar_actors():
     contacts = [Contact("Matthew", "McConaughey", "1969/07/14"), 
                 Contact("Michael", "Caine", "1933/07/14"),
                 Contact("Jessica", "Chastain", "1977/07/14" )]
@@ -16,7 +16,7 @@ def test_1():
     assert("Happy birthday, dear Jessica!", messages[2])
 
     
-def test_2():
+def test_wishing_birthday_for_three_batman_the_dark_knight_rises_actors():
     contacts = [Contact("Christian", "Bale", "1974/07/14"), 
                 Contact("Gary", "Oldman", "1958/07/14"),
                 Contact("Tom", "Hardy", "1977/07/14" )]
