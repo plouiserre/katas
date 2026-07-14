a081d29f47ab8c1242e2c166ab78abe49af4db24
plouiserre <plouiserre@gmail.com>
2026-07-14 17:32:30 +0200

    add person entity

---
+++ BirthdayGreetings/Person.py
@@ -0,0 +1,8 @@
+class Person : 
+    def __init__(self, first_name, last_name, birthday):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birthday = birthday
+
+    def __eq__(self, other):
+        return self.first_name == other.first_name and self.last_name == other.last_name and self.birthday == other.birthday
--- BirthdayGreetings/tests/test_a.py
+++ BirthdayGreetings/tests/test_a.py
@@ -1,42 +1,44 @@
 from datetime import datetime
+from typing import Iterator
+
+from BirthdayGreetings.Person import Person
 
 def test_1():    
-    people = [("Matt", "Damon", "1970/10/08" ), 
-     ("Anne", "Hathaway", "1982/11/12"),
-     ("Tom", "Holland", "1996/06/01")]
-    assert ([("Anne", "Hathaway", "1982/11/12")] == __get_birthdays(people, "11/12"))
+    people = [Person("Matt", "Damon", "1970/10/08" ), 
+     Person("Anne", "Hathaway", "1982/11/12"),
+     Person("Tom", "Holland", "1996/06/01")]
+    assert ([Person("Anne", "Hathaway", "1982/11/12")] == __get_birthdays(people, "11/12"))
 
 def test_2():
-    people = [("Cillian", "Murphy", "1976/05/25" ), 
-     ("Emily", "Blunt", "1983/02/23"),
-     ("Robert", "Downey", "1965/04/04")]
-    assert ([("Cillian", "Murphy", "1976/05/25" )] == __get_birthdays(people, "05/25"))
+    people = [Person("Cillian", "Murphy", "1976/05/25" ), 
+     Person("Emily", "Blunt", "1983/02/23"),
+     Person("Robert", "Downey", "1965/04/04")]
+    assert ([Person("Cillian", "Murphy", "1976/05/25" )] == __get_birthdays(people, "05/25"))
 
 def test_3():
-    people = [("John-David", "Washington", "1984/07/28" ), 
-     ("Robert", "Pattinson", "1986/05/13"),
-     ("Elizabeth", "Debicki", "1990/08/24")]
+    people = [Person("John-David", "Washington", "1984/07/28" ), 
+     Person("Robert", "Pattinson", "1986/05/13"),
+     Person("Elizabeth", "Debicki", "1990/08/24")]
     assert ([] == __get_birthdays(people, "08/25"))
 
 def test_4():
-    people = [("Fionn", "Whitehead", "1997/07/18" ), 
-     ("Tom", "Glynn-Carney", "1995/02/07"),
-     ("Jack", "Lowden", "1990/06/02")]
+    people = [Person("Fionn", "Whitehead", "1997/07/18" ), 
+     Person("Tom", "Glynn-Carney", "1995/02/07"),
+     Person("Jack", "Lowden", "1990/06/02")]
     assert ([] == __get_birthdays(people, "08/18"))
 
 def test_5():
-    people = [("Demi", "Moore", "1962/11/11"), 
-     ("Tom", "Glynn-Carney", "1995/02/07"),
-     ("Leonardo", "DiCaprio", "1974/11/11")]
-    assert ([("Demi", "Moore", "1962/11/11"),("Leonardo", "DiCaprio", "1974/11/11")] == __get_birthdays(people, "11/11"))
+    people = [Person("Demi", "Moore", "1962/11/11"), 
+     Person("Tom", "Glynn-Carney", "1995/02/07"),
+     Person("Leonardo", "DiCaprio", "1974/11/11")]
+    assert ([Person("Demi", "Moore", "1962/11/11"),Person("Leonardo", "DiCaprio", "1974/11/11")] == __get_birthdays(people, "11/11"))
 
 
-def __get_birthdays(people, date_to_celebrate_str) -> list[tuple[str,str,str]]: 
+def __get_birthdays(people : Iterator[Person], date_to_celebrate_str:str) -> list[tuple[str,str,str]]: 
     date_to_celebrate = datetime.strptime(date_to_celebrate_str, '%m/%d').date()
     birthday_humans = []
     for human in people :
-        birthday_date_str = human[2]
-        birthday_date = datetime.strptime(birthday_date_str, '%Y/%m/%d').date()
+        birthday_date = datetime.strptime(human.birthday, '%Y/%m/%d').date()
         if date_to_celebrate.day == birthday_date.day and date_to_celebrate.month == birthday_date.month : 
             birthday_humans.append(human)
     return birthday_humans
