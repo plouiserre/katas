e54726f05b57d6b7a558b252e75b777a2b7f6241
plouiserre <plouiserre@gmail.com>
2026-07-15 14:33:14 +0200

    finish leap year rules implementation

--- BirthdayGreetings/AddressBook.py
+++ BirthdayGreetings/AddressBook.py
@@ -4,14 +4,21 @@ from typing import Iterator
 from BirthdayGreetings.Contact import Contact
 
 class AddressBook : 
-    def __init__(self, all_contacts : Iterator[Contact]):
+    def __init__(self, all_contacts : Iterator[Contact], date_of_the_day):
         self.all_contacts = all_contacts
+        self.date_of_the_day = date_of_the_day
 
     def search_birthday_persons_in_this_date(self, date_str : str): 
-        date_to_celebrate = datetime.strptime(date_str, '%m/%d').date()
+        date_to_celebrate = datetime.strptime(date_str, '%Y/%m/%d').date()
+        is_leap_year_problem = self.__is_a_leap_year()
         birthday_contacts = []
         for contact in self.all_contacts :
             birthday_date = datetime.strptime(contact.birthday, '%Y/%m/%d').date()
-            if date_to_celebrate.day == birthday_date.day and date_to_celebrate.month == birthday_date.month : 
+            if birthday_date.day == 29 and birthday_date.month == 2 and date_to_celebrate.day == 28 and date_to_celebrate.month == 2 and is_leap_year_problem == False :
+                birthday_contacts.append(contact)
+            elif date_to_celebrate.day == birthday_date.day and date_to_celebrate.month == birthday_date.month : 
                 birthday_contacts.append(contact)
         return birthday_contacts
+    
+    def __is_a_leap_year(self):
+        return self.date_of_the_day.is_date_belongs_to_a_leap_of_year()
--- BirthdayGreetings/tests/test_address_book.py
+++ BirthdayGreetings/tests/test_address_book.py
@@ -2,38 +2,58 @@ from typing import Iterator
 
 from BirthdayGreetings.AddressBook import AddressBook
 from BirthdayGreetings.Contact import Contact
+from BirthdayGreetings.DateOfTheDay import DateOfTheDay
 
 def test_search_contact_with_11_12_birthday_and_return_anne_hathaway():    
     people = [Contact("Matt", "Damon", "1970/10/08" ), 
      Contact("Anne", "Hathaway", "1982/11/12"),
      Contact("Tom", "Holland", "1996/06/01")]
-    assert ([Contact("Anne", "Hathaway", "1982/11/12")] == __get_birthdays(people, "11/12"))
+    assert ([Contact("Anne", "Hathaway", "1982/11/12")] == __get_birthdays(people, "2026/11/12"))
 
 def test_search_contact_with_05_25_birthday_and_return_cillian_murphy():
     people = [Contact("Cillian", "Murphy", "1976/05/25" ), 
      Contact("Emily", "Blunt", "1983/02/23"),
      Contact("Robert", "Downey", "1965/04/04")]
-    assert ([Contact("Cillian", "Murphy", "1976/05/25" )] == __get_birthdays(people, "05/25"))
+    assert ([Contact("Cillian", "Murphy", "1976/05/25" )] == __get_birthdays(people, "2026/05/25"))
 
 def test_search_contact_with_good_month_and_bad_day_birthday_and_return_no_one():
     people = [Contact("John-David", "Washington", "1984/07/28" ), 
      Contact("Robert", "Pattinson", "1986/05/13"),
      Contact("Elizabeth", "Debicki", "1990/08/24")]
-    assert ([] == __get_birthdays(people, "08/25"))
+    assert ([] == __get_birthdays(people, "2026/08/25"))
 
 def test_search_contact_with_bad_month_and_good_day_birthday_and_return_no_one():
     people = [Contact("Fionn", "Whitehead", "1997/07/18" ), 
      Contact("Tom", "Glynn-Carney", "1995/02/07"),
      Contact("Jack", "Lowden", "1990/06/02")]
-    assert ([] == __get_birthdays(people, "08/18"))
+    assert ([] == __get_birthdays(people, "2026/08/18"))
 
 def test_search_contact_with_11_11_birthday_and_return_demi_moore_and_leonardo_dicaprio():
     people = [Contact("Demi", "Moore", "1962/11/11"), 
      Contact("Tom", "Glynn-Carney", "1995/02/07"),
      Contact("Leonardo", "DiCaprio", "1974/11/11")]
-    assert ([Contact("Demi", "Moore", "1962/11/11"),Contact("Leonardo", "DiCaprio", "1974/11/11")] == __get_birthdays(people, "11/11"))
+    assert ([Contact("Demi", "Moore", "1962/11/11"),Contact("Leonardo", "DiCaprio", "1974/11/11")] == __get_birthdays(people, "2026/11/11"))
+
+def test_search_contact_with_29_02_birthday_in_a_leap_year_the_28_02():
+    people = [Contact("Clark", "Kent", "1988/09/30"), 
+     Contact("Diana", "Prince", "1992/02/29"),
+     Contact("Bruce", "Wayne", "1989/03/28")]
+    assert ([] == __get_birthdays(people, "2028/02/28"))
+
+def test_search_contact_with_29_02_birthday_in_a_leap_year_the_29_02():
+    people = [Contact("Clark", "Kent", "1988/09/30"), 
+     Contact("Diana", "Prince", "1992/02/29"),
+     Contact("Bruce", "Wayne", "1989/03/28")]
+    assert ([Contact("Diana", "Prince", "1992/02/29")] == __get_birthdays(people, "2028/02/29"))
+
+def test_search_contact_with_29_02_birthday_in_normal_year_the_28_02():
+    people = [Contact("Clark", "Kent", "1988/09/30"), 
+     Contact("Diana", "Prince", "1992/02/29"),
+     Contact("Bruce", "Wayne", "1989/03/28")]
+    assert ([Contact("Diana", "Prince", "1992/02/29")] == __get_birthdays(people, "2026/02/28"))
 
 
 def __get_birthdays(people : Iterator[Contact], date_to_celebrate_str:str) -> list[Contact]: 
-    address_book = AddressBook(people)
+    date_of_the_year = DateOfTheDay(date_to_celebrate_str)
+    address_book = AddressBook(people, date_of_the_year)
     return address_book.search_birthday_persons_in_this_date(date_to_celebrate_str)
