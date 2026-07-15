3709623b8900ef00277fdf3066cd6269364e109d
plouiserre <plouiserre@gmail.com>
2026-07-15 15:48:59 +0200

    create files and reading content

--- BirthdayGreetings/main.py
+++ BirthdayGreetings/main.py
@@ -1,1 +1,7 @@
-print("loooooooooooooooooooooooooooooooooooooool")
+with open("BirthdayGreetings/data/contacts.txt", "r") as file : 
+    content = file.read()
+    print(content)
+
+with open("BirthdayGreetings/data/contacts.json", "r") as file : 
+    content = file.read()
+    print(content)
