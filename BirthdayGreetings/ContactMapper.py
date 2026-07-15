8abac5e7b266cbf63f4898644244f50d8f3a692b
plouiserre <plouiserre@gmail.com>
2026-07-15 16:56:38 +0200

    create ContactMapper et use it

---
+++ BirthdayGreetings/ContactMapper.py
@@ -0,0 +1,22 @@
+import json
+from typing import Iterator
+
+from BirthdayGreetings.Contact import Contact
+
+def json_to_contacts(json_contacts :str) ->Iterator[Contact]:
+    contacts = []
+    clean_txt = json_contacts.replace("\n", "")
+    datas = json.loads(clean_txt)
+    for data_contact in datas : 
+        contacts.append(Contact(data_contact["firstname"], data_contact["lastname"], data_contact["date of birth"]))
+    return contacts
+
+def txt_to_contacts(txt_contacts : str) -> Iterator[Contact] :
+    lines = txt_contacts.split("\n") 
+    contacts = []
+    for line in lines : 
+        if line != '':
+            elements = line.split("|")
+            new_contact = Contact(elements[0],elements[1], elements[2])
+            contacts.append(new_contact)
+    return contacts
--- BirthdayGreetings/tests/test_d.py
+++ BirthdayGreetings/tests/test_d.py
@@ -1,34 +1,14 @@
-import json
-from typing import Iterator
-
 from BirthdayGreetings.Contact import Contact
+from BirthdayGreetings.ContactMapper import json_to_contacts, txt_to_contacts
+
 def test_1():
     text = "Matt|Damon|1970/10/8\nTom|Holland|1996/6/1\nAnne|Hathaway|1982/11/12\nRobert|Pattinson|1986/5/13\nLupita|Nyong'o|1983/3/1\nSamantha|Morton|1977/5/13\n"
     assert([Contact("Matt", "Damon", "1970/10/8"), Contact("Tom", "Holland", "1996/6/1"), Contact("Anne", "Hathaway", "1982/11/12"),
             Contact("Robert", "Pattinson", "1986/5/13"), Contact("Lupita", "Nyong'o", "1983/3/1"), 
-            Contact("Samantha", "Morton", "1977/5/13")] == transform_text_from_txt_to_list_contact(text))
-
-
-def transform_text_from_txt_to_list_contact(text : str) -> Iterator[Contact]:
-    lines = text.split("\n") 
-    contacts = []
-    for line in lines : 
-        if line != '':
-            elements = line.split("|")
-            new_contact = Contact(elements[0],elements[1], elements[2])
-            contacts.append(new_contact)
-    return contacts
+            Contact("Samantha", "Morton", "1977/5/13")] == txt_to_contacts(text))
 
 def test_2():
     text = '[{"firstname":"Matt", "lastname":"Damon", "date of birth":"1970/10/8"},\n{"firstname":"Tom", "lastname":"Holland", "date of birth":"1996/6/1"},\n{"firstname":"Anne", "lastname":"Hathaway", "date of birth":"1982/11/12"},\n{"firstname":"Robert", "lastname":"Pattinson", "date of birth":"1986/5/13"},\n{"firstname":"Lupita", "lastname":"Nyong\'o", "date of birth":"1983/3/1"},\n{"firstname":"Samantha", "lastname":"Morton", "date of birth":"1977/5/13"}\n]'
     assert([Contact("Matt", "Damon", "1970/10/8"), Contact("Tom", "Holland", "1996/6/1"), Contact("Anne", "Hathaway", "1982/11/12"),
             Contact("Robert", "Pattinson", "1986/5/13"), Contact("Lupita", "Nyong'o", "1983/3/1"), 
-            Contact("Samantha", "Morton", "1977/5/13")] == transform_text_from_json_to_list_contact(text))
-
-def transform_text_from_json_to_list_contact(text : str) -> Iterator[Contact]:
-    contacts = []
-    clean_txt = text.replace("\n", "")
-    datas = json.loads(clean_txt)
-    for data_contact in datas : 
-        contacts.append(Contact(data_contact["firstname"], data_contact["lastname"], data_contact["date of birth"]))
-    return contacts
+            Contact("Samantha", "Morton", "1977/5/13")] == json_to_contacts(text))
