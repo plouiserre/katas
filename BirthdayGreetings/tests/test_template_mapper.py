e5524eb9e353500e3df808d02aac9c83d1c82c58
plouiserre <plouiserre@gmail.com>
2026-07-16 16:31:51 +0200

    implement storage and reading of message whishing

---
+++ BirthdayGreetings/tests/test_template_mapper.py
@@ -0,0 +1,10 @@
+from BirthdayGreetings.Template import Template
+from BirthdayGreetings.TemplateMapper import json_to_template, txt_to_template
+
+def test_map_template_from_txt_file_to_template():
+    text = "Happy birthday, dear <first_name>!|<first_name>"
+    assert(Template("Happy birthday, dear <first_name>!", "<first_name>") == txt_to_template(text))
+
+def test_map_template_from_json_file_to_template():
+    text = '{"message":"Happy birthday, dear <first_name>!", "template":"<first_name>"}'
+    assert(Template("Happy birthday, dear <first_name>!", "<first_name>") == json_to_template(text))
