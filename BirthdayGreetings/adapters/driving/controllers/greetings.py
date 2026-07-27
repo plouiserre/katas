a16cb3390f0184a46d5b2f534c7872941f7dc517
plouiserre <plouiserre@gmail.com>
2026-07-27 15:32:47 +0200

    ok for run cli and api

--- BirthdayGreetings/adapters/driving/controllers/greetings.py
+++ BirthdayGreetings/adapters/driving/controllers/greetings.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from BirthdayGreetings.adapters.driven.ContactManagerJson import ContactManagerJson
 from BirthdayGreetings.adapters.driven.TemplateManagerJson import TemplateManagerJson
 from BirthdayGreetings.domain.BirthdayCollaborators import BirthdayCollaborators
-from BirthdayGreetings.domain.DateOfTheDay import DateOfTheDay
 
 router = APIRouter()
 
@@ -11,8 +10,7 @@ router = APIRouter()
 async def get_greetings(year : str, month : str, day : str):
     date_to_evaluate = year+"/"+month+"/"+day
     contact_manager_txt = ContactManagerJson()
-    date_of_the_day = DateOfTheDay(date_to_evaluate)
     template_manager_txt = TemplateManagerJson()
-    birthday_collaborators = BirthdayCollaborators(contact_manager_txt, date_of_the_day, template_manager_txt)
+    birthday_collaborators = BirthdayCollaborators(contact_manager_txt, template_manager_txt)
     messages_birthday_employees = birthday_collaborators.GreetingsBirthday(date_to_evaluate)
     return {"all messages for birthday":messages_birthday_employees}
