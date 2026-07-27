cfe6cad464e83a1d2c79a6eedc5717976e156c85
plouiserre <plouiserre@gmail.com>
2026-07-27 14:32:57 +0200

    rename test for birthday

--- BirthdayGreetings/tests/domain/test_contact_.py.py
+++ BirthdayGreetings/tests/domain/test_contact_.py.py
@@ -1,29 +1,35 @@
 from BirthdayGreetings.domain.Contact import Contact
 from BirthdayGreetings.domain.DateOfTheDay import DateOfTheDay
 
-def test_1(): 
+def test_find_it_is_birthday_for_john_doe(): 
     date_to_study = "2026/07/27"
     contact = Contact("John", "Doe", "1989/07/27")
     is_birthday = __it_is_birthday_contact(contact, date_to_study)
     assert(is_birthday == True)
 
-def test_2(): 
+def test_find_it_is_not_birthday_for_john_doe(): 
     date_to_study = "2026/07/26"
     contact = Contact("John", "Doe", "1989/07/27")
     is_birthday = __it_is_birthday_contact(contact, date_to_study)
     assert(is_birthday == False)
 
-def test_3():
+def test_find_it_is_birthday_for_john_doe_because_it_is_leap_year():
     date_to_study = "2026/02/28"
     contact = Contact("John", "Doe", "1988/02/29")
     is_birthday = __it_is_birthday_contact(contact, date_to_study)
     assert(is_birthday == True)
 
-def test_4():
+def test_find_it_is_not_birthday_for_john_doe_because_it_is_not_leap_year():
     date_to_study = "2024/02/28"
     contact = Contact("John", "Doe", "1988/02/29")
     is_birthday = __it_is_birthday_contact(contact, date_to_study)
     assert(is_birthday == False)
+
+def test_find_it_is_not_birthday_for_john_doe_during_a_leap_year():
+    date_to_study = "2024/02/29"
+    contact = Contact("John", "Doe", "1988/02/29")
+    is_birthday = __it_is_birthday_contact(contact, date_to_study)
+    assert(is_birthday == True)
 
 def __it_is_birthday_contact(contact : Contact, date_to_study_str : str): 
     date_of_the_day = DateOfTheDay(date_to_study_str)
