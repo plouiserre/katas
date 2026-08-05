a8a214f131d8ac28a65c0363dbc36cce2b94213d
plouiserre <plouiserre@gmail.com>
2026-08-05 14:57:17 +0200

    implement response in cli

--- SocialNetwork/adapters/driving/cli/cli_get_all_posts.py
+++ SocialNetwork/adapters/driving/cli/cli_get_all_posts.py
@@ -1,12 +1,14 @@
+from SocialNetwork.adapters.driving.response.wall_response import WallResponse
+
 class CliGetAllPosts: 
     def __init__(self, wall):
         self.wall = wall
 
     def run_get_all_posts(self):
-        all_posts = self.wall.get_all_messages_from_all_accounts() 
-        if all_posts == {} :
+        posts_from_specific_user = self.wall.get_all_messages_from_all_accounts() 
+        if posts_from_specific_user == {} :
             print("None message")
         else : 
-            for post in all_posts : 
-                    date_posting = post.date_posting.strftime("%d/%m/%y %H:%M:%S")
-                    print(post.author.name+": "+date_posting+" "+post.content_message)  
+            wall_response = WallResponse.to_response(posts_from_specific_user)
+            for post in wall_response.posts : 
+                print(post.author.name+": "+post.date_posting+": "+post.content_message)            
--- SocialNetwork/adapters/driving/cli/cli_search.py
+++ SocialNetwork/adapters/driving/cli/cli_search.py
@@ -1,3 +1,5 @@
+from SocialNetwork.adapters.driving.response.wall_response import WallResponse
+
 class CliSearch: 
     def __init__(self, arguments, search_service):
         self.arguments = arguments 
@@ -6,12 +8,13 @@ class CliSearch:
     def run_search_command(self): 
         if len(self.arguments) == 1 : 
             account_name = self.arguments[0]
-            messages_from_specific_user =  self.search_service.load_wall_and_run_search_posts_from_specific_user(account_name)
-            if messages_from_specific_user == [] : 
+            posts_from_specific_user =  self.search_service.load_wall_and_run_search_posts_from_specific_user(account_name)
+            if posts_from_specific_user == [] : 
                 print("none message from "+account_name)
             else : 
                 print ("messages de "+account_name)
-                for message in messages_from_specific_user : 
-                    print(message.content_message)
+                wall_response = WallResponse.to_response(posts_from_specific_user)
+                for post in wall_response.posts : 
+                    print(post.date_posting+": "+post.content_message)
         else : 
             print("Commande invalide")
