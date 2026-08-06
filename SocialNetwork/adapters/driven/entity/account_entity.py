a6cafd0518e4b8e9602c9e5bf3b56a0ad53e9508
plouiserre <plouiserre@gmail.com>
2026-08-06 16:09:33 +0200

    authorentity -> accountentity

--- SocialNetwork/adapters/driven/entity/account_entity.py
+++ SocialNetwork/adapters/driven/entity/account_entity.py
@@ -4,17 +4,17 @@ from dataclasses import dataclass
 from SocialNetwork.domain.models.account import Account
 
 @dataclass(frozen=True)
-class AuthorEntity :
+class AccountEntity :
     name : str
 
     @staticmethod
     def create_to_entity(author : Account):
-        return AuthorEntity(author.name)
+        return AccountEntity(author.name)
 
     @staticmethod
-    def create_to_domain(author_entity : AuthorEntity):
+    def create_to_domain(author_entity : AccountEntity):
         return Account(author_entity.name, [])
 
     @staticmethod
     def create_to_entity_from_message_json(datas):
-        return AuthorEntity(datas["author"]["name"])
+        return AccountEntity(datas["author"]["name"])
--- SocialNetwork/adapters/driven/entity/wall_entity.py
+++ SocialNetwork/adapters/driven/entity/wall_entity.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 
-from SocialNetwork.adapters.driven.entity.author_entity import AuthorEntity
+from SocialNetwork.adapters.driven.entity.account_entity import AccountEntity
 from SocialNetwork.adapters.driven.entity.post_entity import PostEntity
 from SocialNetwork.domain.models.wall import Wall
 
@@ -29,7 +29,7 @@ class WallEntity :
     def create_to_entity_from_wall_json(datas):
         all_posts = []
         for post in datas["posts"] : 
-            author = AuthorEntity.create_to_entity_from_message_json(post)
+            author = AccountEntity.create_to_entity_from_message_json(post)
             post_entity = PostEntity.create_to_entity_from_message_json(author, post)
             all_posts.append(post_entity)
         wall_entity = WallEntity(all_posts)
