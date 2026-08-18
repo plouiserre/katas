b7501a147c59fd8f2919daccbf2d3549f1f70f98
Pierre-Louis Serré <pierre-louisserre@device-291.home>
2026-08-18 15:50:57 +0200

    all logic developed in tests

--- Tricount/tests/test_e.py
+++ Tricount/tests/test_e.py
@@ -1,7 +1,12 @@
+from dataclasses import dataclass
+
 import math
+import pytest
 
 from  decimal import Decimal
 from enum import Enum
+
+from Tricount.cannot_divide_by_zero_exception import CannotDivideByZeroException
 
 def test_1(): 
     final_money = (MoneyDriver(RoundedType.BELOW)
@@ -52,45 +57,137 @@ def test_8():
                    .divide("20", "2.54")
                    .display_final_result())
     assert("7.88" == final_money)
+    
+def test_9():
+    final_money = (MoneyDriver(RoundedType.BELOW)
+                   .add("9.87", "2.32")
+                   .substract(None, "43.23")
+                   .multiply(None, "93.23")
+                   .divide(None, "7.3")
+                   .display_final_result())
+    assert("-396.42" == final_money)
 
+def test_10():
+    final_money = (MoneyDriver(RoundedType.UP)
+                   .add("9.87", "2.32")
+                   .substract(None, "43.23")
+                   .multiply(None, "93.23")
+                   .divide(None, "7.3")
+                   .display_final_result())
+    assert("-396.41" == final_money)
+    
+def test_11():
+    final_money = (MoneyDriver(RoundedType.BELOW)
+                   .add("9.87", "2.32")
+                   .substract(None, "3.23")
+                   .multiply(None, "93.23")
+                   .divide(None, "7.3")
+                   .display_final_result())
+    assert("114.43" == final_money)
+
+def test_12():
+    final_money = (MoneyDriver(RoundedType.UP)
+                   .add("9.87", "2.32")
+                   .substract(None, "3.23")
+                   .multiply(None, "93.23")
+                   .divide(None, "7.3")
+                   .display_final_result())
+    assert("114.44" == final_money)
+
+def test_13():
+    with pytest.raises(CannotDivideByZeroException) :
+        (MoneyDriver(RoundedType.BELOW)
+                   .divide("89.2", "0"))
+def test_14():
+    with pytest.raises(CannotDivideByZeroException) :
+        (MoneyDriver(RoundedType.BELOW)
+                   .add("93", "2")
+                   .divide("89.2", "0"))
 
 class MoneyDriver : 
     def __init__(self, rounded_type):
         self.final_result = Decimal("0.0")
         self.rounded_type = rounded_type
+        self.all_ops = []
 
     def add(self, first_number_str : str, second_number_str : str): 
-        first_number = Decimal(first_number_str)
-        second_number = Decimal(second_number_str)
-        self.final_result = first_number + second_number
+        if first_number_str != None : 
+            self.all_ops.append(Operation(first_number_str, second_number_str, Operand.Add, False))
+        else : 
+            self.all_ops.append(Operation(None, second_number_str, Operand.Add, True))
         return self
 
     def substract(self, first_number_str : str, second_number_str : str): 
-        first_number = Decimal(first_number_str)
-        second_number = Decimal(second_number_str)
-        self.final_result = first_number - second_number
+        if first_number_str != None : 
+            self.all_ops.append(Operation(first_number_str, second_number_str, Operand.Soustract, False))
+        else : 
+            self.all_ops.append(Operation(None, second_number_str, Operand.Soustract, True))      
         return self
 
     def multiply(self, first_number_str : str, second_number_str : str): 
-        first_number = Decimal(first_number_str)
-        second_number = Decimal(second_number_str)
-        self.final_result = first_number * second_number
+        if first_number_str != None : 
+            self.all_ops.append(Operation(first_number_str, second_number_str, Operand.Multiply, False))
+        else : 
+            self.all_ops.append(Operation(None, second_number_str, Operand.Multiply, True))      
         return self
 
     def divide(self, first_number_str : str, second_number_str : str):
-        first_number = Decimal(first_number_str)
-        second_number = Decimal(second_number_str)
-        self.final_result = first_number / second_number
+        if second_number_str == "0":
+            raise CannotDivideByZeroException("Cannot divide by 0")
+        elif first_number_str != None : 
+            self.all_ops.append(Operation(first_number_str, second_number_str, Operand.Divide, False))
+        else : 
+            self.all_ops.append(Operation(None, second_number_str, Operand.Divide, True))      
         return self
 
     def display_final_result(self): 
         display = ""
+        last_result = self.__calculate_all_operations()
         if self.rounded_type == RoundedType.BELOW  : 
-            display = str(math.floor(self.final_result * 100)/100.0)
+            display = str(math.floor(last_result * 100)/100.0)
         else : 
-            display = str(math.ceil(self.final_result * 100)/100.0)
+            display = str(math.ceil(last_result * 100)/100.0)
         return display
+    
+    def __calculate_all_operations(self):
+        last_result = Decimal("0")
+        for op in self.all_ops : 
+            if op.operand == Operand.Add:
+                if op.is_last_result_needed == False : 
+                    last_result = Decimal(op.first_number) + Decimal(op.second_number) 
+                else : 
+                    last_result = last_result + Decimal(op.second_number)
+            elif op.operand == Operand.Soustract : 
+                if op.is_last_result_needed == False : 
+                    last_result = Decimal(op.first_number) - Decimal(op.second_number) 
+                else : 
+                    last_result = last_result - Decimal(op.second_number)
+            elif op.operand == Operand.Multiply : 
+                if op.is_last_result_needed == False : 
+                    last_result = Decimal(op.first_number) * Decimal(op.second_number) 
+                else : 
+                    last_result = last_result * Decimal(op.second_number) 
+            else : 
+                if op.is_last_result_needed == False : 
+                    last_result = Decimal(op.first_number) / Decimal(op.second_number) 
+                else :
+                    last_result = last_result / Decimal(op.second_number)
+        return last_result
+                
 
 class RoundedType(Enum):
     BELOW = -1
     UP = 1
+    
+class Operand(Enum): 
+    Add = 1
+    Soustract = 2
+    Multiply = 3
+    Divide = 4
+    
+@dataclass
+class Operation(): 
+    first_number : str
+    second_number : str
+    operand : Operand
+    is_last_result_needed : bool
