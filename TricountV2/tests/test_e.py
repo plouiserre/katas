a55e7dd4582399a7415cef4fea4d59a3acd558b0
plouiserre <plouiserre@gmail.com>
2026-08-24 19:31:33 +0200

    wip

--- TricountV2/tests/test_e.py
+++ TricountV2/tests/test_e.py
@@ -6,19 +6,37 @@ from TricountV2.MoneyLogic.money import Money
 from TricountV2.participant import Participant
 
 def test_1() : 
-    refund_calculated =(ParticipantDriver()
+    refund_between_participants_calculated =(ParticipantDriver()
               .add_payer(Participant.create_participant("Jean", "-50.35", None, RoundedType.BELOW))
               .add_participant_to_refund(Participant.create_participant("Fantine", "50.35", None, RoundedType.BELOW))
-              .get_refund())
+              .get_refund_and_participants_updated())
     refund_expected = Refund.create_refund("Jean", "Fantine", "50.35")
-    assert(compare_refund(refund_expected, refund_calculated))  
+    refund_between_participants_expected = RefundBetweenParticipants.create(refund_expected, Participant.create_participant("Jean", "0", None, RoundedType.BELOW), Participant.create_participant("Fantine", "0", None, RoundedType.BELOW))
+    assert(compare_refund(refund_between_participants_expected, refund_between_participants_calculated))  
 
-def compare_refund(refund_expected : Refund, refund_calculated : Refund):
-    is_equal = refund_expected.amount == refund_calculated.amount and refund_expected.recipient == refund_calculated.recipient and refund_expected.payer == refund_calculated.payer
-    return is_equal 
+
+# def test_2() : 
+#     refund_calculated =(ParticipantDriver()
+#               .add_payer(Participant.create_participant("Jean", "-50.35", None, RoundedType.BELOW))
+#               .add_participant_to_refund(Participant.create_participant("Fantine", "100.66", None, RoundedType.BELOW))
+#               .get_refund())
+#     refund_expected = Refund.create_refund("Jean", "Fantine", "50.35")
+#     assert(compare_refund(refund_expected, refund_calculated))  
+
+def compare_refund(refund_between_participants_expected : RefundBetweenParticipants, refund_between_participants_calculated : RefundBetweenParticipants):
+    refund_expected = refund_between_participants_expected.refund
+    refund_calculated = refund_between_participants_calculated.refund
+    payer_expected = refund_between_participants_expected.payer
+    payer_calculated = refund_between_participants_calculated.payer
+    recipiant_expected = refund_between_participants_expected.recipient
+    recipiant_calculated = refund_between_participants_calculated.recipient
+    is_equal_refund = refund_expected.amount == refund_calculated.amount and refund_expected.recipient == refund_calculated.recipient and refund_expected.payer == refund_calculated.payer
+    is_equal_payer = payer_expected.name == payer_calculated.name and payer_expected.balance == payer_calculated.balance
+    is_equal_recipiant = recipiant_expected.name == recipiant_calculated.name and recipiant_calculated.balance == recipiant_expected.balance
+    return is_equal_refund and  is_equal_payer and is_equal_recipiant
 
 # TODO faire : 
-# 1 - payer.balance = refund.balance OK 
+# 1 - payer.balance = refund.balance  DOING 
 # 2 - payer.balance < refund.balance 
 # 3 - payer.balance > refund.balance 
 # 4 - externaliser code 
@@ -29,19 +47,32 @@ def compare_refund(refund_expected : Refund, refund_calculated : Refund):
 class ParticipantDriver(): 
     def __init__(self):
         self.payer = None
-        self.participant_to_refund = None
+        self.recipient = None
 
     def add_payer(self, participant : Participant): 
         self.payer = participant
         return self
 
     def add_participant_to_refund(self, participant : Participant):
-        self.participant_to_refund = participant
+        self.recipient = participant
         return self
 
-    def get_refund(self):
+    def get_refund_and_participants_updated(self):
         balance_payer = self.payer.balance.replace("-", "")
-        return Refund.create_refund(self.payer.name, self.participant_to_refund.name, balance_payer)
+        refund = Refund.create_refund(self.payer.name, self.recipient.name, balance_payer)
+        self.payer.balance = "0"
+        self.recipient.balance = "0"
+        return RefundBetweenParticipants.create(refund, self.payer, self.recipient)
+
+@dataclass
+class RefundBetweenParticipants :
+    refund : Refund
+    payer : Participant 
+    recipient : Participant
+
+    @staticmethod
+    def create(refund, payer, recipient):
+        return RefundBetweenParticipants(refund, payer, recipient)
 
 @dataclass(frozen=True)
 class Refund : 
