0476a32885ef713eeaeebab781a84998cb2955b0
Pierre-Louis Serré <pierre-louisserre@device-291.home>
2026-08-25 18:43:30 +0200

    tests ok

---
+++ TricountV2/tests/test_g.py
@@ -0,0 +1,69 @@
+from TricountV2.activity import Activity
+from TricountV2.balance_calculator import BalanceCalculator
+from TricountV2.manage_refunds import ManageRefunds
+from TricountV2.refund import Refund
+
+def test_1():
+    refunds_calculated = (TripDriver()
+                            .add_activity("bar", 23.5, ["harry", "hermione"], "harry")
+                            .calculate_refunds())
+    refunds_expected = [Refund.create_refund("hermione", "harry", "11.75")]
+    assert(len(refunds_calculated) == 1)
+    assert(compare_all_refunds(refunds_expected, refunds_calculated)) 
+
+def test_2():
+    refunds_calculated = (TripDriver()
+                            .add_activity("bar", 23.5, ["harry", "hermione"], "harry")
+                            .add_activity("restaurant", 50.7, ["hermione", "ron"], "hermione")
+                            .add_activity("bowling", 12, ["ron", "harry"], "ron")
+                            .calculate_refunds()
+                            )
+    refunds_expected = [Refund.create_refund("ron", "hermione", "13.6"), Refund.create_refund("ron", "harry", "5.75")]
+    assert(len(refunds_calculated) == 2)
+    assert(compare_all_refunds(refunds_expected, refunds_calculated)) 
+    
+def test_3():
+    refunds_calculated = (TripDriver()
+                            .add_activity("bar", 23.5, ["harry", "hermione", "ron"], "harry")
+                            .add_activity("restaurant", 50.7, ["hermione", "ron", "ginny", "hagrid"], "hermione")
+                            .add_activity("bowling", 12, ["ron", "ginny"], "ron")
+                            .add_activity("plane tickets", 1200, ["harry", "hermione", "ron", "ginny", "hagrid"], "ginny")
+                            .add_activity("hotel", 615, ["harry", "hermione", "ron", "ginny","hagrid"], "hagrid")
+                            .add_activity("spa day", 120, ["hermione", "ginny"], "hermione")
+                            .calculate_refunds())
+    
+    refunds_expected = [Refund.create_refund("ron", "ginny", "377.51"), Refund.create_refund("harry", "ginny", "347.34"),
+                        Refund.create_refund("hermione", "ginny", "33.47"), Refund.create_refund("hermione", "hagrid", "239.32")]
+    assert(len(refunds_calculated) == 4)
+    assert(compare_all_refunds(refunds_expected, refunds_calculated)) 
+    
+def compare_all_refunds(refunds_expected : Refund, refunds_calculated : Refund):
+    is_equal = True
+    for idx, _ in enumerate(refunds_expected): 
+        refund_expected = refunds_expected[idx]
+        refund_calculated = refunds_calculated[idx]
+        is_equal = refund_expected.amount == refund_calculated.amount and refund_expected.recipient == refund_calculated.recipient and refund_expected.payer == refund_calculated.payer
+        if is_equal == False : 
+            break
+    return is_equal 
+    
+    
+class TripDriver : 
+    def __init__(self):
+        self.activities = []
+        pass
+    
+    def add_activity(self, name_activity, price, participants, payer):
+        activity  = Activity.create(name_activity, price, participants, payer)
+        self.activities.append(activity)
+        return self
+    
+    def calculate_refunds(self):
+        balance_calculator = BalanceCalculator()
+        manage_refunds = ManageRefunds()
+        for activity in self.activities :
+            balance_calculator.add_activity(activity.name, activity.price, activity.participants_name, activity.payer)
+        participants = balance_calculator.calculate_participants_balance_from_activities()
+        for participant in participants : 
+            manage_refunds.add_participant(participant)
+        return manage_refunds.calculate_all_refunds()
