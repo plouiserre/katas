ec1bed62ae586c8aa4ff671b32591765c0f3bb73
plouiserre <plouiserre@gmail.com>
2026-08-26 14:26:41 +0200

    cli finish

--- TricountV2/cli/cli_app.py
+++ TricountV2/cli/cli_app.py
@@ -1,4 +1,4 @@
-from TricountV2.cli.cli_command import CliCommand
+from TricountV2.cli.cli_activity import CliActivity
 from TricountV2.business.trip import Trip
 
 class cliApp :
@@ -8,14 +8,29 @@ class cliApp :
     def run(self): 
         while True : 
             raw = input("> ")
-            cli_command = CliCommand(raw)
-            command, arguments = cli_command.find_command_and_arguments()
+            command = self.__get_command(raw)
             if raw == "stop":
                 break
             elif command == "activity": 
+                cli_activity = CliActivity(raw)
+                arguments = cli_activity.decompose_arguments()
                 if(len(arguments) != 4):
                     print("activité incomplète")
-                self.trip.add_activity(arguments[0], arguments[1], arguments[2], arguments[3])
+                _all_participants = self.__get__participants(arguments[2])
+                self.trip.add_activity(arguments[0], arguments[1], _all_participants, arguments[3])
+            elif command == "calculate": 
+                all_refunds = self.trip.calculate_refunds()
+                for refund in all_refunds : 
+                    print(refund.payer+" paye "+refund.amount+" à "+refund.recipient)    
+            else : 
+                print("commande inconnue")
+
+    def __get_command(self, command_complete): 
+        return command_complete.split()[0]
+
+    def __get__participants(self, participants_glued): 
+        all_participants = participants_glued.replace("_", " ").split()
+        return all_participants
 #             command, arguments = cli_command.find_command_and_arguments()
 #             if raw == "stop":
 #                 break
