a0f4292327ef4f8b2f1ff418db70d241598abf4a
Pierre-Louis Serré <pierre-louisserre@device-291.home>
2026-08-26 12:24:57 +0200

    working cli command

--- TricountV2/cli/cli_command.py
+++ TricountV2/cli/cli_command.py
@@ -1,31 +1,51 @@
+from TricountV2.cli.CommandBadFormattingException import CommandBadFormattingException
+
 class CliCommand : 
     def __init__(self, cli_command):
         self.cli_command = cli_command
 
     def find_command_and_arguments(self):
-        indexs  = self.__find_quotation_in_command()
-        if len(indexs) > 1 : 
-            return self.__manage_commands_with_quotes(indexs)
-        else : 
-            args = self.cli_command.split(" ")
-            if len(args) > 1 : 
-                if len(args) == 2 :
-                    return args[0], [args[1]]
-                else : 
-                    return args[0], [args[1], args[2]]
-            else : 
-                return self.cli_command, []
-
-    def __manage_commands_with_quotes(self, indexs): 
-        message_to_post = self.cli_command[indexs[0]+1:indexs[1]]
-        cli_command_without_big_argument = self.cli_command.replace(message_to_post, "").replace("\"", "").strip()
-        elements_cli_command = cli_command_without_big_argument.split(" ")
-        args = [elements_cli_command[1], message_to_post]
-        return elements_cli_command[0], args
-
-    def __find_quotation_in_command(self) -> list[int] :
-        indexs = []
+        arguments_between_brackets_with_brackets = self.__find_arguments_with_brackets()
+        arguments_between_brackets_transformed = self.__transform_argument_between_bracket()
+        self.cli_command = self.cli_command.replace(arguments_between_brackets_with_brackets, arguments_between_brackets_transformed)
+        command, arguments = self.__split_arguments()
+        return command, arguments
+    
+    def __find_arguments_with_brackets(self):
+        indexs = self.__find_indexs_brackets()
+        brackets_content_with_brackets = self.cli_command[indexs[0]:indexs[1]+1]
+        return brackets_content_with_brackets
+    
+    def __transform_argument_between_bracket(self): 
+        brackets_arguments = self.__find_arguments_between_brackets()
+        brackets_arguments = ' '.join(brackets_arguments.split())
+        brakets_arguments_glu = brackets_arguments.strip().replace(" ","_")
+        return brakets_arguments_glu
+    
+    def __find_arguments_between_brackets(self):
+        indexs = self.__find_indexs_brackets()
+        brakets_arguments = self.cli_command[indexs[0]+1:indexs[1]]
+        return brakets_arguments
+    
+    def __find_indexs_brackets(self):
+        indexs_first_caracter = []
+        indexs_second_caracter = []
         for index , caracter in enumerate(self.cli_command) : 
-            if caracter == "\"": 
-                indexs.append(index)
+            if caracter == "[": 
+                indexs_first_caracter.append(index)
+        for index , caracter in enumerate(self.cli_command) : 
+                    if caracter == "]": 
+                        indexs_second_caracter.append(index)
+        if len(indexs_first_caracter) != 1  or len(indexs_second_caracter) != 1: 
+            raise CommandBadFormattingException("arguments are bad formatting")
+        indexs = [indexs_first_caracter[0], indexs_second_caracter[0]]
         return indexs
+    
+    def __split_arguments(self):
+        args = self.cli_command.split(" ")
+        command = args[0]
+        args.remove(args[0])
+        arguments = []
+        for arg in args :
+            arguments.append(arg)
+        return command, arguments
