a9fba8504b795b1833899697e9ce2f3eaced0542
plouiserre <plouiserre@gmail.com>
2026-08-27 10:53:32 +0200

    mv code

--- PokerHands/tests/draw/test_h.py
+++ PokerHands/tests/draw/test_h.py
@@ -1,6 +1,7 @@
 import random
 
 from PokerHands.card import Card, CardColor, CardValue
+from PokerHands.draw.draw_card import DrawCard
 
 def test_1():
     card_spread = (CardSpreadDriver()
@@ -8,22 +9,36 @@ def test_1():
                    .get_card_spread_and_rest_cards())
     
     assert(card_spread.value in CardValue)
+    assert(card_spread.value != CardValue.UNDEFINED)
     assert(card_spread.color in CardColor)
+    assert(card_spread.color != CardColor.UNDEFINED)
+
+def test_2(): 
+    i = 0
+    while i < 200 : 
+        card_spread = (CardSpreadDriver()
+                           .spread_one_card()
+                           .get_card_spread_and_rest_cards())
+            
+        assert(card_spread.value in CardValue)
+        assert(card_spread.value != CardValue.UNDEFINED)
+        assert(card_spread.color in CardColor)
+        assert(card_spread.color != CardColor.UNDEFINED)
+        i+= 1
     
     
 class CardSpreadDriver():
     def __init__(self):
-        self.deck_cards = self.__build_all_cards()
+        self.deck_cards = self.__build_deck_cards()
+        self.draw_card = DrawCard()
      
     def spread_one_card(self):
         return self
     
     def get_card_spread_and_rest_cards(self):
-        idx_card_spread = random.randint(0, len(self.deck_cards) - 1)
-        card_spread = self.deck_cards[idx_card_spread]
-        return card_spread
+        return self.draw_card.pick_one(self.deck_cards)
     
-    def __build_all_cards(self):
+    def __build_deck_cards(self):
         all_cards = []
         for card_value in CardValue:
             if card_value == CardValue.UNDEFINED : 
