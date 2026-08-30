52559e04149a0dacfbed62d1af5bea7bd69a5ee8
plouiserre <plouiserre@gmail.com>
2026-08-30 19:46:42 +0200

    mv code

--- PokerHandsV2/tests/test_k.py
+++ PokerHandsV2/tests/test_k.py
@@ -1,9 +1,6 @@
 import pytest
 
 from PokerHands.card import Card, CardColor, CardValue
-from PokerHands.AllFigures.Figure import Figure
-from PokerHandsV2.AllFigures.HighCardFigure import HighCardFigure
-from PokerHandsV2.AllFigures.PairFigure import PairFigure
 from PokerHandsV2.counting_cards import CountingCards
 from PokerHandsV2.detector.four_cards_detector import FourCardsDetector
 from PokerHandsV2.detector.flush_detector import FlushDetector
@@ -18,7 +15,7 @@ from PokerHandsV2.draw.multi_draw_cards import MultiDrawCards
 from PokerHandsV2.exception.PlayerDoNotHaveCompleteHandException import PlayerDoNotHaveCompleteHandException
 from PokerHandsV2.exception.TooManyPlayerException import TooManyPlayerException
 from PokerHandsV2.hand import Hand
-from PokerHandsV2.winner import Winner
+from PokerHandsV2.hands_manager import HandsManager
 
 def test_1(): 
     player_with_better_hand = (DrawAndComparePlayersHandDriver()
@@ -262,86 +259,22 @@ class DrawAndComparePlayersHandDriver():
         full_detector = FullDetector(counting_cards)
         four_cards_detector = FourCardsDetector(counting_cards)
         quinte_flush_detector = QuinteFlushDetector()
-        self.hand = Hand(high_card_detector, pair_detector, two_pairs_detector, three_cards_detector, straight_detector, flush_detector, full_detector, four_cards_detector, quinte_flush_detector)
-        self.multi_draw_cards = MultiDrawCards()
+        hand = Hand(high_card_detector, pair_detector, two_pairs_detector, three_cards_detector, straight_detector, flush_detector, full_detector, four_cards_detector, quinte_flush_detector)
         self.players = {}
+        multi_draw_cards = MultiDrawCards()
+        self.hands_manager = HandsManager(hand, multi_draw_cards)
         
     def add_player(self, name_player):
-        self.players[name_player] = []
+        self.hands_manager.add_player(name_player)
         return self
 
     def draw_card_player(self, name_player):
-        new_card = self.multi_draw_cards.draw_one_card()
-        self.players[name_player].append(new_card)
+        self.hands_manager.draw_card_player(name_player)
         return self
 
     def give_specific_hand(self, name_player, cards): 
-        self.players[name_player] = cards
+        self.hands_manager.give_specific_hand(name_player, cards)
         return self
 
     def determine_player_with_better_hand(self):
-        self.__check_all_players_have_all_their_cards()
-        self.__check_not_too_many_players()
-        hands_by_player = {}
-        best_hand = None
-        best_players = []
-        for player_name in self.players : 
-            player = self.players[player_name]
-            hand = self.hand.determinate_high_figure(player) 
-            hands_by_player[player_name] = hand
-        for player_name in hands_by_player : 
-            hand = hands_by_player[player_name]
-            if best_hand == None : 
-                best_hand = hand
-                continue
-            hands_to_compare = [hand, best_hand]
-            winner = self.__determinate_winner(hands_to_compare)
-            if winner == Winner.FIRST_HAND :
-                best_hand = hand
-        for player_name in hands_by_player : 
-            hand = hands_by_player[player_name] 
-            if best_hand == hand : 
-                best_players.append(player_name)
-        return best_players
-
-    def __determinate_winner(self, hands : list[Figure] ): 
-        first_hand = hands[0]
-        second_hand = hands[1]
-        if first_hand.points < second_hand.points : 
-            return Winner.SECOND_HAND
-        elif second_hand.points < first_hand.points : 
-            return Winner.FIRST_HAND
-        elif type(first_hand) is HighCardFigure and type(second_hand) is HighCardFigure:
-            return self.__compare_two_hands_with_high_cards(first_hand, second_hand)
-        elif type(first_hand) is PairFigure and type(second_hand) is PairFigure: 
-            return self.__compare_two_hands_with_pairs(first_hand, second_hand)
-
-    def __compare_two_hands_with_high_cards(self, first_hand : Hand, second_hand : Hand) -> Winner: 
-            if first_hand.value < second_hand.value : 
-                return Winner.SECOND_HAND
-            elif second_hand.value < first_hand.value : 
-                return Winner.FIRST_HAND 
-            else :
-                return Winner.EQUALITY
-            
-    def __compare_two_hands_with_pairs(self, first_hand : Hand, second_hand : Hand) -> Winner:
-        if first_hand.value < second_hand.value : 
-            return Winner.SECOND_HAND
-        elif second_hand.value < first_hand.value : 
-            return Winner.FIRST_HAND
-        else : 
-            if first_hand.high_value_rest_of_cards < second_hand.high_value_rest_of_cards : 
-                return Winner.SECOND_HAND
-            elif second_hand.high_value_rest_of_cards < first_hand.high_value_rest_of_cards :
-                return Winner.FIRST_HAND
-            else : 
-                return Winner.EQUALITY
-
-    def __check_all_players_have_all_their_cards(self): 
-        for player_name in self.players : 
-            if len(self.players[player_name]) < 2: 
-                raise PlayerDoNotHaveCompleteHandException(player_name+" do not have his/hers 2 cards ")
-            
-    def __check_not_too_many_players(self): 
-        if len(self.players) > 10 : 
-            raise TooManyPlayerException("You cannot have more than ten players for a game.")
+        return self.hands_manager.get_players_with_best_hands()   
