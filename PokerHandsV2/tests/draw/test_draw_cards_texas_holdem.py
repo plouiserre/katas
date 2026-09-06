fffa0bb085ae814de93ca83bb42cead1c8bc7c58
plouiserre <plouiserre@gmail.com>
2026-09-06 18:17:47 +0200

    test_draw_cards_texas_holdem.py rework

--- PokerHandsV2/tests/draw/test_draw_cards_texas_holdem.py
+++ PokerHandsV2/tests/draw/test_draw_cards_texas_holdem.py
@@ -3,97 +3,67 @@ from PokerHandsV2.draw.draw_cards_texas_holdem import DrawCardsTexasHoldem
 from dataclasses import dataclass
 
 def test_draw_all_cards_need_for_two_players_in_poker_texas_holdem(): 
-    cards = (
-        DrawCardsTexasHoldemDriver()
-            .add_one_player("Bruce")
-            .add_one_player("Diana")
-            .give_one_card_for_one_player("Bruce")
-            .give_one_card_for_one_player("Diana")
-            .give_one_card_for_one_player("Bruce")
-            .give_one_card_for_one_player("Diana")
-            .burn_one_card()
-            .draw_flop_cards()
-            .burn_one_card()
-            .draw_turn_card()
-            .burn_one_card()
-            .draw_river_card()
-            .get_all_cards_choosen()
-    )
-    is_burn_card_ok = True
-    all_cards_drawn = __get_all_cards_drawn(cards)
-    all_cards_drawn_unique = set(all_cards_drawn)
-    assert(len(cards.players["Bruce"]) == 2)
-    assert(len(cards.players["Diana"]) == 2)
-    assert(len(cards.cards_on_table) == 5)
-    assert (len(cards.burn_cards) == 3)
-    for card in cards.burn_cards :
-        is_burn_card_ok = card not in cards.players["Bruce"] and card not in cards.players["Diana"] and card not in cards.cards_on_table
-        if is_burn_card_ok == False : 
-            break
-    assert (is_burn_card_ok == True)
-    assert (len(all_cards_drawn) == len(all_cards_drawn_unique))
-
-
+    (DrawCardsTexasHoldemDriver()
+        .add_one_player("Bruce")
+        .add_one_player("Diana")
+        .give_one_card_for_one_player("Bruce")
+        .give_one_card_for_one_player("Diana")
+        .give_one_card_for_one_player("Bruce")
+        .give_one_card_for_one_player("Diana")
+        .burn_one_card()
+        .draw_flop_cards()
+        .burn_one_card()
+        .draw_turn_card()
+        .burn_one_card()
+        .draw_river_card()
+        .get_all_cards_choosen2()
+        .is_player_have_all_cards("Bruce")
+        .is_player_have_all_cards("Diana")
+        .is_cards_on_table_are_completed()
+        .is_cards_burned_are_completed()
+        .is_cards_drawned_are_uniqued())
+    
 def test_draw_all_cards_need_for_six_players_in_poker_texas_holdem(): 
-    cards = (
-        DrawCardsTexasHoldemDriver()
-            .add_one_player("Bruce")
-            .add_one_player("Diana")
-            .add_one_player("Clark")
-            .add_one_player("Selina")
-            .add_one_player("Barry")
-            .add_one_player("Zatana")
-            .give_one_card_for_one_player("Bruce")
-            .give_one_card_for_one_player("Diana")
-            .give_one_card_for_one_player("Clark")
-            .give_one_card_for_one_player("Selina")
-            .give_one_card_for_one_player("Barry")
-            .give_one_card_for_one_player("Zatana")
-            .give_one_card_for_one_player("Bruce")
-            .give_one_card_for_one_player("Diana")
-            .give_one_card_for_one_player("Clark")
-            .give_one_card_for_one_player("Selina")
-            .give_one_card_for_one_player("Barry")
-            .give_one_card_for_one_player("Zatana")
-            .burn_one_card()
-            .draw_flop_cards()
-            .burn_one_card()
-            .draw_turn_card()
-            .burn_one_card()
-            .draw_river_card()
-            .get_all_cards_choosen()
-    )
-    is_burn_card_ok = True
-    all_cards_drawn = __get_all_cards_drawn(cards)
-    all_cards_drawn_unique = set(all_cards_drawn)
-    assert(len(cards.players["Bruce"]) == 2)
-    assert(len(cards.players["Diana"]) == 2)
-    assert(len(cards.players["Clark"]) == 2)
-    assert(len(cards.players["Selina"]) == 2)
-    assert(len(cards.players["Barry"]) == 2)
-    assert(len(cards.players["Zatana"]) == 2)
-    assert(len(cards.cards_on_table) == 5)
-    assert (len(cards.burn_cards) == 3)
-    for card in cards.burn_cards :
-        is_burn_card_ok = card not in cards.players["Bruce"] and card not in cards.players["Diana"] and card not in cards.players["Clark"] and card not in cards.players["Selina"] and card not in cards.players["Barry"] and card not in cards.players["Zatana"] and card not in cards.cards_on_table 
-        if is_burn_card_ok == False : 
-            break
-    assert (is_burn_card_ok == True)
-    assert (len(all_cards_drawn) == len(all_cards_drawn_unique))
-        
-
-def __get_all_cards_drawn(cards):
-    all_cards_drawn = []
-    for player_name in cards.players : 
-        for card in cards.players[player_name]:
-            all_cards_drawn.append(card)
-    for card in cards.cards_on_table : 
-        all_cards_drawn.append(card)
-    return all_cards_drawn
+    (DrawCardsTexasHoldemDriver()
+        .add_one_player("Bruce")
+        .add_one_player("Diana")
+        .add_one_player("Clark")
+        .add_one_player("Selina")
+        .add_one_player("Barry")
+        .add_one_player("Zatana")
+        .give_one_card_for_one_player("Bruce")
+        .give_one_card_for_one_player("Diana")
+        .give_one_card_for_one_player("Clark")
+        .give_one_card_for_one_player("Selina")
+        .give_one_card_for_one_player("Barry")
+        .give_one_card_for_one_player("Zatana")
+        .give_one_card_for_one_player("Bruce")
+        .give_one_card_for_one_player("Diana")
+        .give_one_card_for_one_player("Clark")
+        .give_one_card_for_one_player("Selina")
+        .give_one_card_for_one_player("Barry")
+        .give_one_card_for_one_player("Zatana")
+        .burn_one_card()
+        .draw_flop_cards()
+        .burn_one_card()
+        .draw_turn_card()
+        .burn_one_card()
+        .draw_river_card()
+        .get_all_cards_choosen2()
+        .is_player_have_all_cards("Bruce")
+        .is_player_have_all_cards("Diana")
+        .is_player_have_all_cards("Clark")
+        .is_player_have_all_cards("Selina")
+        .is_player_have_all_cards("Barry")
+        .is_player_have_all_cards("Zatana")        
+        .is_cards_on_table_are_completed()
+        .is_cards_burned_are_completed()
+        .is_cards_drawned_are_uniqued())    
 
 class DrawCardsTexasHoldemDriver : 
     def __init__(self):
         self.draw_cards_texas_holdem = DrawCardsTexasHoldem()
+        self.cards = None
 
     def add_one_player(self, name_player):
         self.draw_cards_texas_holdem.add_players(name_player)
@@ -124,6 +94,47 @@ class DrawCardsTexasHoldemDriver :
         all_cards_on_table = self.draw_cards_texas_holdem.get_all_cards_on_table()
         burns_cards = self.draw_cards_texas_holdem.get_all_burns_cards()
         return Cards(all_players, all_cards_on_table, burns_cards)
+
+    def get_all_cards_choosen2(self):
+        all_players = self.draw_cards_texas_holdem.get_players()
+        all_cards_on_table = self.draw_cards_texas_holdem.get_all_cards_on_table()
+        burns_cards = self.draw_cards_texas_holdem.get_all_burns_cards()
+        self.cards = Cards(all_players, all_cards_on_table, burns_cards)
+        return self
+
+    def is_player_have_all_cards(self, player_name_search):
+        cards_search = []
+        for player_name in self.cards.players : 
+            if player_name == player_name_search:
+                cards_search = self.cards.players[player_name]
+        assert(len(cards_search) == 2)
+        return self
+
+    def is_cards_on_table_are_completed(self):
+        assert(len(self.cards.cards_on_table) == 5)
+        return self
+
+    def is_cards_burned_are_completed(self):
+        assert(len(self.cards.burn_cards) == 3)
+        return self
+
+    def is_cards_burned_are_uniqued(self):
+        is_burn_card_ok = True
+        for card in self.cards.burn_cards :
+            is_burn_card_ok = card not in self.cards.players["Bruce"] and card not in self.cards.players["Diana"] and card not in self.cards.cards_on_table
+            if is_burn_card_ok == False : 
+                break
+        assert(is_burn_card_ok)
+
+    def is_cards_drawned_are_uniqued(self):
+        all_cards_drawn = []
+        for player_name in self.cards.players : 
+            for card in self.cards.players[player_name]:
+                all_cards_drawn.append(card)
+        for card in self.cards.cards_on_table : 
+            all_cards_drawn.append(card)
+        all_cards_drawn_unique = set(all_cards_drawn)
+        assert (len(all_cards_drawn) == len(all_cards_drawn_unique))
 
 @dataclass(frozen=True)
 class Cards :
