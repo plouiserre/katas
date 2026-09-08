6972aad05897c9cf6ff7a1446717efbd983dfe50
plouiserre <plouiserre@gmail.com>
2026-09-08 12:20:16 +0200

    finish rewrite new tests poker with only fonctionnel

--- PokerHandsV2/tests/game/test_draw_phase.py
+++ PokerHandsV2/tests/game/test_draw_phase.py
@@ -19,94 +19,78 @@ from PokerHandsV2.game.hands_manager import HandsManager
 from PokerHandsV2.tests.fake_multi_draw_cards import FakeMultiDrawCards
 
 def test_launch_draw_phase_with_two_players_randomly(): 
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(MultiDrawCards())
-                               .add_players(["Steve","Natacha"])    
-                               .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Steve"] or player_with_better_hand == ["Natacha"] or player_with_better_hand == ["Steve", "Natacha"])
-
+    (DrawAndComparePlayersHandDriver(MultiDrawCards())
+        .add_players(["Steve","Natacha"])    
+        .launch_draw_phase_and_compare_players_hand()
+        .is_this_players_can_be_a_winner(["Steve", "Natacha", "Steve_Natacha"]))
+    
 def test_launch_draw_phase_with_two_players_and_steve_wins(): 
-    fake_cards = [Card(CardValue.KING, CardColor.CLUBS), Card(CardValue.QUEEN, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.DIAMONDS),Card(CardValue.SEVEN, CardColor.SPADES)]
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                   .add_players(["Steve","Natacha"])    
-                                   .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Steve"])
+    fake_cards = ["K♣","Q♥","Q♦","7♠"]
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+        .add_players(["Steve","Natacha"])    
+        .launch_draw_phase_and_compare_players_hand()
+        .is_this_players_can_be_a_winner(["Steve"]))
 
 def test_launch_draw_phase_with_two_players_and_natacha_wins(): 
-    fake_cards = [Card(CardValue.TEN, CardColor.CLUBS), Card(CardValue.QUEEN, CardColor.HEARTS), Card(CardValue.JACK, CardColor.DIAMONDS), Card(CardValue.SEVEN, CardColor.SPADES)]
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                   .add_players(["Steve","Natacha"])                                 
-                                   .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Natacha"])
-
+    fake_cards = ["10♣","Q♥","J♦","7♠"]
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+            .add_players(["Steve","Natacha"])                                 
+            .launch_draw_phase_and_compare_players_hand()
+            .is_this_players_can_be_a_winner(["Natacha"]))
+    
 def test_launch_draw_phase_with_two_players_and_steve_and_natacha_win(): 
-    fake_cards = [Card(CardValue.QUEEN, CardColor.CLUBS), Card(CardValue.QUEEN, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.DIAMONDS), Card(CardValue.QUEEN, CardColor.SPADES)]
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                   .add_players(["Steve","Natacha"])    
-                                   .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Steve", "Natacha"])
+    fake_cards = ["Q♣","Q♥","Q♦","Q♠"]
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+        .add_players(["Steve","Natacha"])    
+        .launch_draw_phase_and_compare_players_hand()
+        .is_this_players_can_be_a_winner(["Steve_Natacha"]))
 
 def test_launch_draw_phase_with_six_players_randomly(): 
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(MultiDrawCards())
-                               .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])                                                                          
-                               .launch_draw_phase_and_compare_players_hand())
-    assert("Steve" in  player_with_better_hand or "Natacha" in  player_with_better_hand or "Tony" in  player_with_better_hand 
-           or "Thor" in  player_with_better_hand  or "Bruce" in  player_with_better_hand  or "Clint" in  player_with_better_hand )
+    (DrawAndComparePlayersHandDriver(MultiDrawCards())
+        .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])                                                                          
+        .launch_draw_phase_and_compare_players_hand()
+        .is_this_players_can_be_a_winner(["Steve", "Natacha", "Tony", "Thor", "Bruce", "Clint"]))
 
 def test_launch_draw_phase_with_six_players_and_steve_wins():
-    fake_cards = [Card(CardValue.KING, CardColor.CLUBS), Card(CardValue.QUEEN, CardColor.HEARTS), Card(CardValue.JACK, CardColor.CLUBS), Card(CardValue.TEN, CardColor.HEARTS), 
-                  Card(CardValue.NINE, CardColor.CLUBS), Card(CardValue.EIGHT, CardColor.HEARTS), Card(CardValue.QUEEN, CardColor.DIAMONDS), Card(CardValue.JACK, CardColor.SPADES),
-                  Card(CardValue.TEN, CardColor.DIAMONDS), Card(CardValue.NINE, CardColor.SPADES), Card(CardValue.EIGHT, CardColor.DIAMONDS), Card(CardValue.SEVEN, CardColor.SPADES)]
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                       .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])                                                                                                                        
-                                       .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Steve"])
+    fake_cards = ["K♣", "Q♥", "J♣", "10♥",  "9♣", "8♥", "Q♦", "J♠", "10♦", "9♠", "8♦", "7♠"]
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+            .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])                                                                                                                        
+            .launch_draw_phase_and_compare_players_hand()
+            .is_this_players_can_be_a_winner(["Steve"]))
 
 def test_launch_draw_phase_with_six_players_and_tony_wins():
-    fake_cards = [Card(CardValue.SEVEN, CardColor.CLUBS), Card(CardValue.JACK, CardColor.CLUBS), Card(CardValue.QUEEN, CardColor.HEARTS), Card(CardValue.TEN, CardColor.HEARTS),
-                  Card(CardValue.NINE, CardColor.CLUBS), Card(CardValue.EIGHT, CardColor.HEARTS), Card(CardValue.SIX, CardColor.DIAMONDS), Card(CardValue.TEN, CardColor.DIAMONDS), 
-                  Card(CardValue.JACK, CardColor.SPADES), Card(CardValue.NINE, CardColor.SPADES), Card(CardValue.EIGHT, CardColor.DIAMONDS), Card(CardValue.SEVEN, CardColor.SPADES)]
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                       .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])                                                                                                                      
-                                       .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Tony"])
+    fake_cards = ["7♣", "J♣", "Q♥", "10♥", "9♣", "8♥", "6♦", "10♦", "J♠", "9♠", "8♦", "7♠"]
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+            .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])                                                                                                                      
+            .launch_draw_phase_and_compare_players_hand()
+            .is_this_players_can_be_a_winner(["Tony"]))
 
 def test_launch_draw_phase_with_six_players_and_clint_wins():
-    fake_cards = [Card(CardValue.SEVEN, CardColor.CLUBS), Card(CardValue.SIX, CardColor.HEARTS), Card(CardValue.JACK, CardColor.CLUBS), Card(CardValue.TEN, CardColor.HEARTS),
-                  Card(CardValue.NINE, CardColor.CLUBS), Card(CardValue.ACE, CardColor.HEARTS), Card(CardValue.SIX, CardColor.DIAMONDS), Card(CardValue.FIVE, CardColor.SPADES),
-                  Card(CardValue.TEN, CardColor.DIAMONDS), Card(CardValue.NINE, CardColor.SPADES), Card(CardValue.EIGHT, CardColor.DIAMONDS), Card(CardValue.ACE , CardColor.SPADES)]
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                       .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])
-                                       .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Clint"])
+    fake_cards = ["7♣", "6♥", "J♣", "10♥", "9♣", "A♥", "6♦", "5♠", "10♦", "9♠", "8♦", "A♠"]
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+            .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])
+            .launch_draw_phase_and_compare_players_hand()
+            .is_this_players_can_be_a_winner(["Clint"]))
 
 def test_launch_draw_phase_with_six_players_and_natacha_and_bruce_win(): 
-    fake_cards = [Card(CardValue.SEVEN, CardColor.CLUBS), Card(CardValue.KING, CardColor.HEARTS), Card(CardValue.JACK, CardColor.CLUBS), Card(CardValue.TEN, CardColor.HEARTS),
-                  Card(CardValue.KING, CardColor.CLUBS),Card(CardValue.TWO, CardColor.HEARTS), Card(CardValue.SIX, CardColor.DIAMONDS), Card(CardValue.QUEEN, CardColor.SPADES),
-                  Card(CardValue.TEN, CardColor.DIAMONDS), Card(CardValue.NINE, CardColor.SPADES), Card(CardValue.QUEEN, CardColor.DIAMONDS), Card(CardValue.FOUR , CardColor.SPADES)]
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                           .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])
-                                           .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Natacha", "Bruce"])
+    fake_cards = ["7♣", "K♥", "J♣", "10♥", "K♣", "2♥", "6♦", "Q♠", "10♦", "9♠", "Q♦", "4♠"]
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+            .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint"])
+            .launch_draw_phase_and_compare_players_hand()
+            .is_this_players_can_be_a_winner(["Natacha_Bruce"]))
 
 def test_launch_draw_phase_with_ten_players_randomly(): 
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(MultiDrawCards())
-                                    .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint","Carol","T'Challa","Steven","Wanda"])
-                                    .launch_draw_phase_and_compare_players_hand())
-    assert("Steve" in  player_with_better_hand or "Natacha" in  player_with_better_hand or "Tony" in  player_with_better_hand 
-           or "Thor" in  player_with_better_hand  or "Bruce" in  player_with_better_hand  or "Clint" in  player_with_better_hand 
-           or "Carol" in player_with_better_hand or "T'Challa" in player_with_better_hand or "Steven" in player_with_better_hand
-           or "Peter" in player_with_better_hand or "Wanda" in player_with_better_hand)
+    (DrawAndComparePlayersHandDriver(MultiDrawCards())
+        .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint","Carol","T'Challa","Steven","Wanda"])
+        .launch_draw_phase_and_compare_players_hand()
+        .is_this_players_can_be_a_winner(["Steve", "Natacha", "Tony", "Thor", "Bruce", "Clint", "Carol", "T'Challa", "Steven", "Peter", "Wanda"]))
 
 def test_launch_draw_phase_with_ten_players_and_carol_wins():
-    fake_cards = [Card(CardValue.SEVEN, CardColor.CLUBS), Card(CardValue.KING, CardColor.HEARTS), Card(CardValue.JACK, CardColor.CLUBS), Card(CardValue.TEN, CardColor.HEARTS),
-                  Card(CardValue.KING, CardColor.CLUBS), Card(CardValue.TWO, CardColor.HEARTS), Card(CardValue.ACE, CardColor.HEARTS), Card(CardValue.ACE, CardColor.DIAMONDS), 
-                  Card(CardValue.THREE, CardColor.SPADES), Card(CardValue.KING, CardColor.SPADES), Card(CardValue.SIX, CardColor.DIAMONDS), Card(CardValue.QUEEN, CardColor.SPADES),
-                  Card(CardValue.TEN, CardColor.DIAMONDS), Card(CardValue.NINE, CardColor.SPADES), Card(CardValue.QUEEN, CardColor.DIAMONDS), Card(CardValue.FOUR , CardColor.SPADES),
-                  Card(CardValue.ACE , CardColor.SPADES), Card(CardValue.QUEEN , CardColor.SPADES), Card(CardValue.FOUR , CardColor.CLUBS), Card(CardValue.KING , CardColor.DIAMONDS)] 
-    player_with_better_hand = (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
-                                .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint","Carol","T'Challa","Steven","Wanda"])
-                                .launch_draw_phase_and_compare_players_hand())
-    assert(player_with_better_hand == ["Carol"])
+    fake_cards = ["7♣", "K♥", "J♣", "10♥", "K♣", "2♥", "A♥", "A♦", "3♠", "K♠", "6♦", "Q♠", "10♦", "9♠", "Q♦", "4♠","A♠", "Q♠", "4♣", "K♦"] 
+    (DrawAndComparePlayersHandDriver(FakeMultiDrawCards(fake_cards))
+        .add_players(["Steve","Natacha","Tony","Thor","Bruce","Clint","Carol","T'Challa","Steven","Wanda"])
+        .launch_draw_phase_and_compare_players_hand()
+        .is_this_players_can_be_a_winner(["Carol"]))
 
 def test_launch_draw_phase_with_eleven_players_and_the_game_fails(): 
     with pytest.raises(TooManyPlayerException) :
@@ -130,6 +114,7 @@ class DrawAndComparePlayersHandDriver():
         self.players = {}
         self.multi_draw_cards = multidrawcards
         self.hands_manager = HandsManager(hand, self.multi_draw_cards)
+        self.winners = []
         
     def add_players(self, name_players):
         for name_player in name_players:
@@ -138,4 +123,18 @@ class DrawAndComparePlayersHandDriver():
 
     def launch_draw_phase_and_compare_players_hand(self):
         self.draw_phase = DrawPhase(self.players, self.hands_manager)
-        return self.draw_phase.launch_phase_and_get_best_players()   
+        self.winners =  self.draw_phase.launch_phase_and_get_best_players()
+        return self
+
+    def is_this_players_can_be_a_winner(self, players_name):
+        is_winner = False
+        for player_name in players_name : 
+            if "_" in player_name : 
+                all_players = player_name.split("_")
+                is_winner = all_players == self.winners
+            else : 
+                is_winner = player_name in self.winners
+            if is_winner == True: 
+                break
+        assert (is_winner == True)
+        return self   
