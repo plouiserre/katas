0642cd7d2f1048ac6266ddb8e81983b7d7504932
plouiserre <plouiserre@gmail.com>
2026-09-09 17:56:40 +0200

    mv code

--- PokerHandsV2/manipulating_cards.py
+++ PokerHandsV2/manipulating_cards.py
@@ -1,4 +1,6 @@
-from PokerHands.card import Card, CardValue
+import copy
+
+from PokerHands.card import Card, CardValue, CardColor
 from typing import Iterator
 
 class ManipulatingCards :
@@ -13,3 +15,20 @@ class ManipulatingCards :
             else : 
                 counting_cards[card.value] = 1
         return counting_cards
+
+    def sorted_card(self, hand): 
+        all_cards = copy.deepcopy(hand)
+        hand_ordered = []
+        while(len(hand_ordered) < len(all_cards)) :
+            min_card = Card(CardValue.UNDEFINED, CardColor.UNDEFINED)
+            for idx, card_in_hand in enumerate(hand) : 
+                card = hand[idx]
+                if min_card == Card(CardValue.UNDEFINED, CardColor.UNDEFINED) :
+                    min_card = card
+                    continue
+                else : 
+                    if min_card.value > card.value : 
+                        min_card = card
+            hand_ordered.append(min_card)
+            hand.remove(min_card) 
+        return hand_ordered
--- PokerHandsV2/tests/test_manipulating_cards.py
+++ PokerHandsV2/tests/test_manipulating_cards.py
@@ -7,37 +7,36 @@ def test_count_each_where_different_cards():
     (ManipulatingCardDriver()
             .add_all_cards_crypted(["Q♦", "J♥", "6♠", "A♣", "4♦"])
             .count_card()
-            .validate_number_card("Q", "1")
-            .validate_number_card("J", "1")
-            .validate_number_card("6", "1")
-            .validate_number_card("A", "1")            
-            .validate_number_card("4", "1"))      
+            .is_valid_number_card("Q", "1")
+            .is_valid_number_card("J", "1")
+            .is_valid_number_card("6", "1")
+            .is_valid_number_card("A", "1")            
+            .is_valid_number_card("4", "1"))      
 
 def test_count_each_where_two_same_cards(): 
     (ManipulatingCardDriver()
             .add_all_cards_crypted(["Q♦", "J♥", "6♠", "4♣", "Q♦"] )
             .count_card()
-            .validate_number_card("Q", "2")
-            .validate_number_card("J", "1")
-            .validate_number_card("6", "1")
-            .validate_number_card("4", "1"))   
+            .is_valid_number_card("Q", "2")
+            .is_valid_number_card("J", "1")
+            .is_valid_number_card("6", "1")
+            .is_valid_number_card("4", "1"))   
 
 def test_count_each_where_three_same_cards():
     (ManipulatingCardDriver()
                 .add_all_cards_crypted(["A♦", "10♥", "A♠", "A♣", "6♦"])
                 .count_card()
-                .validate_number_card("A", "3")
-                .validate_number_card("10", "1")
-                .validate_number_card("6", "1"))
+                .is_valid_number_card("A", "3")
+                .is_valid_number_card("10", "1")
+                .is_valid_number_card("6", "1"))
 
 def test_count_each_where_fourth_same_cards():
     (ManipulatingCardDriver()
                     .add_all_cards_crypted(["Q♦", "Q♠", "Q♥", "A♦", "Q♣"])
                     .count_card()
-                    .validate_number_card("Q", "4")
-                    .validate_number_card("A", "1"))
+                    .is_valid_number_card("Q", "4")
+                    .is_valid_number_card("A", "1"))
 
-#♦♥♠♣
 def test_sorted_5_cards_following_each_other():
     (ManipulatingCardDriver()
                     .add_all_cards_crypted(["3♦", "4♥", "5♠", "6♣", "7♦"])
@@ -68,20 +67,12 @@ def test_sorted_5_cards_some_with_same_values_mixed_not_order():
                             .sorted_card()
                             .is_valid_order(["3♦", "6♣", "6♥", "A♠", "A♦"]))
 
-
-
-#TODO tests à faire 
-# 1 - tout de suits dans l'ordre 
-# 2 - tout se suit dans le désordre 
-# 3 - tout se suit mélangé
-# 4 - des trous
-# 5 - plusieurs cartes avec la même valeur
-
 class ManipulatingCardDriver(): 
     def __init__(self):
         self.hand = []
         self.hand_ordered = []
-        self.counting_card = {}        
+        self.counting_card = {}
+        self.manipulating_cards = ManipulatingCards()        
 
     def add_all_cards_crypted(self, all_cards_crypted):
         for card_crypted in all_cards_crypted : 
@@ -90,28 +81,14 @@ class ManipulatingCardDriver():
         return self
 
     def count_card(self):
-        manipulating_cards = ManipulatingCards()
-        self.counting_card =  manipulating_cards.Count(self.hand)
+        self.counting_card =  self.manipulating_cards.Count(self.hand)
         return self
 
     def sorted_card(self): 
-        all_cards = copy.deepcopy(self.hand)
-        self.hand_ordered = []
-        while(len(self.hand_ordered) < len(all_cards)) :
-            min_card = Card(CardValue.UNDEFINED, CardColor.UNDEFINED)
-            for idx, card_in_hand in enumerate(self.hand) : 
-                card = self.hand[idx]
-                if min_card == Card(CardValue.UNDEFINED, CardColor.UNDEFINED) :
-                    min_card = card
-                    continue
-                else : 
-                    if min_card.value > card.value : 
-                        min_card = card
-            self.hand_ordered.append(min_card)
-            self.hand.remove(min_card) 
+        self.hand_ordered = self.manipulating_cards.sorted_card(self.hand)
         return self
 
-    def validate_number_card(self, card_value_crypted, number):
+    def is_valid_number_card(self, card_value_crypted, number):
         is_valid = False
         for card_value in self.counting_card : 
             card_value_asked = Card.parse_value(card_value_crypted)
